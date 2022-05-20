a4dee8e0d5a107915728c09f4f795e51ff2e3a83
Deeka Wong <huangdijia@gmail.com>
2022-05-20 17:49:26 +0800

    Update .phpstorm.meta.php

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -2,15 +2,16 @@
 
 declare(strict_types=1);
 /**
- * This file is part of closure-command.
+ * This file is part of friendsofhyperf/components.
  *
- * @link     https://github.com/friendsofhyperf/closure-command
- * @document https://github.com/friendsofhyperf/closure-command/blob/main/README.md
+ * @link     https://github.com/friendsofhyperf/components
+ * @document https://github.com/friendsofhyperf/components/blob/1.x/README.md
  * @contact  huangdijia@gmail.com
  */
 namespace PHPSTORM_META;
 
     // Reflect
     override(\Psr\Container\ContainerInterface::get(0), map('@'));
+    override(\Hyperf\Context\Context::get(0), map('@'));
     override(\Hyperf\Utils\Context::get(0), map('@'));
     override(\make(0), map('@'));
