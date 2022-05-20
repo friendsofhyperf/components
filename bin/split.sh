60c50f7550ac29a193f332d378e6347c4c24c723
Deeka Wong <huangdijia@gmail.com>
2022-05-20 14:51:02 +0800

    Optimized split.sh

--- bin/split.sh
+++ bin/split.sh
@@ -31,7 +31,7 @@ if [[ $# -eq 0 ]]; then
 fi
 
 for REPO in $REPOS ; do
-    remote $REPO git@github.com:friendsofhyperf/${REPO}.git
+    remote $REPO https://github.com/friendsofhyperf/${REPO}.git
 
     split "src/$REPO" $REPO
 done
