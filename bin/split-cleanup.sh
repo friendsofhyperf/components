bccad74bf17182e9ecb3b3745a984b7e347e8f80
Deeka Wong <huangdijia@gmail.com>
2022-05-20 23:10:04 +0800

    Update split-cleanup.sh

--- bin/split-cleanup.sh
+++ bin/split-cleanup.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
-for i in src/*/.git src/*/.vscode src/*/.github src/*/vendor/ src/*/.gitignore src/*/.php-cs-fixer.php src/*/.phpunit* src/*/composer.lock src/*/phpunit.* src/*/phpstan* src/*/.phpstorm.meta*; do
+SOMETHINGS="src/*/.git src/*/.vscode src/*/.github src/*/vendor/ src/*/.gitignore src/*/.php-cs-fixer.php src/*/.phpunit* src/*/composer.lock src/*/phpunit.* src/*/phpstan* src/*/.phpstorm.meta*"
+
+for i in $SOMETHINGS; do
+    echo "Removing $i"
     rm -rf $i
 done
