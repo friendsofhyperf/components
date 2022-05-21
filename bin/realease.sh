8f3cf71b189c4eea46489b132309e6054d3ea61e
huangdijia <huangdijia@gmail.com>
2022-05-21 22:58:05 +0800

    Update realease.sh

--- bin/realease.sh
+++ bin/realease.sh
@@ -73,7 +73,6 @@ do
     echo "Releasing $REMOTE";
 
     TMP_DIR="/tmp/friendsofhyperf-split"
-    # REMOTE_URL="https://github.com/friendsofhyperf/${REMOTE}"
     REMOTE_URL="git@github.com:friendsofhyperf/$REMOTE.git"
 
     rm -rf $TMP_DIR;
