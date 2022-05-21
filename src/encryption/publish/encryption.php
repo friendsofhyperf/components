a982ef756baae083723242916aeaa58800f9df65
Deeka Wong <huangdijia@gmail.com>
2022-05-21 09:53:53 +0800

    Format

--- src/encryption/publish/encryption.php
+++ src/encryption/publish/encryption.php
@@ -2,10 +2,10 @@
 
 declare(strict_types=1);
 /**
- * This file is part of friendsofhyperf/encryption.
+ * This file is part of friendsofhyperf/components.
  *
- * @link     https://github.com/friendsofhyperf/encryption
- * @document https://github.com/friendsofhyperf/encryption/blob/main/README.md
+ * @link     https://github.com/friendsofhyperf/components
+ * @document https://github.com/friendsofhyperf/components/blob/2.x/README.md
  * @contact  huangdijia@gmail.com
  */
 return [
