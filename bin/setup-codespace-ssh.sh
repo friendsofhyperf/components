b1c60e1ffe775f4e9a60030e9e922cab2961ef25
Deeka Wong <huangdijia@gmail.com>
2022-05-23 16:49:45 +0800

    Update dockerfile

--- bin/setup-codespace-ssh.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-mkdir -p ~/.ssh/ \
-    && echo -e $ID_ED25519_PRIV > ~/.ssh/id_ed25519 \
-    && chown vscode. /home/vscode/.ssh/id_ed25519 \
-    && chmod 600 ~/.ssh/id_ed25519 \
-    && ssh-keyscan github.com >> ~/.ssh/known_hosts
