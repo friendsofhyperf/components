e27f42f3e62b4f96aea3cc85e92b122cf398f244
Deeka Wong <huangdijia@gmail.com>
2022-06-10 15:52:32 +0800

    Update .phpstorm.meta.php

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -11,8 +11,10 @@ declare(strict_types=1);
 namespace PHPSTORM_META;
 
     // Reflect
-    override(\Psr\Container\ContainerInterface::get(0), map('@'));
-    override(\Hyperf\Context\Context::get(0), map('@'));
-    override(\app(0), map('@'));
-    override(\di(0), map('@'));
-    override(\make(0), map('@'));
+    override(\Psr\Container\ContainerInterface::get(0), map(['' => '@']));
+    override(\Hyperf\Context\Context::get(0), map(['' => '@']));
+    override(\app(0), map(['' => '@']));
+    override(\di(0), map(['' => '@']));
+    override(\make(0), map(['' => '@']));
+    override(\optional(0), type(0));
+    override(\tap(0), type(0));
