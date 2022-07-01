edc315e3b343f34462b42c690c7c4ac5b2889bf2
Deeka Wong <huangdijia@gmail.com>
2022-07-01 12:58:05 +0800

    Update

--- bin/gen-markdown-table.sh
+++ bin/gen-markdown-table.sh
@@ -7,5 +7,5 @@ echo "|Repository|Version|Downloads|"
 echo "|--|--|--|"
 
 for REPO in ${REPOS}; do
-    echo "|${REPO}|[![Latest Stable Version](https://img.shields.io/packagist/v/friendsofhyperf/${REPO})](https://packagist.org/packages/friendsofhyperf/${REPO})|[![Total Downloads](https://img.shields.io/packagist/dt/friendsofhyperf/${REPO})](https://packagist.org/packages/friendsofhyperf/${REPO})|"
+    echo "|[${REPO}](https://github.com/friendsofhyperf/${REPO})|[![Latest Stable Version](https://img.shields.io/packagist/v/friendsofhyperf/${REPO})](https://packagist.org/packages/friendsofhyperf/${REPO})|[![Total Downloads](https://img.shields.io/packagist/dt/friendsofhyperf/${REPO})](https://packagist.org/packages/friendsofhyperf/${REPO})|"
 done
