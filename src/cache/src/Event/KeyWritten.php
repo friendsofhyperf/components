7b9bab2b094fff886756c8994399e2cf047c913b
Deeka Wong <huangdijia@gmail.com>
2022-08-10 14:13:19 +0800

    cs fix

--- src/cache/src/Event/KeyWritten.php
+++ src/cache/src/Event/KeyWritten.php
@@ -13,11 +13,9 @@ namespace FriendsOfHyperf\Cache\Event;
 class KeyWritten extends CacheEvent
 {
     /**
-     * @param string $key
      * @param mixed $value
-     * @param null|int $seconds
      */
-    public function __construct($key, public $value, public ?int $seconds = null)
+    public function __construct(string $key, public $value, public ?int $seconds = null)
     {
         parent::__construct($key);
     }
