80d1208475eec47db102ebfdb8f00bda1f5dcaef
Deeka Wong <huangdijia@gmail.com>
2022-08-23 12:07:29 +0800

    Remove useless code

--- src/confd/src/Listener/WatchOnBootListener.php
+++ src/confd/src/Listener/WatchOnBootListener.php
@@ -11,17 +11,13 @@ declare(strict_types=1);
 namespace FriendsOfHyperf\Confd\Listener;
 
 use FriendsOfHyperf\Confd\Confd;
-use Hyperf\Contract\ConfigInterface;
 use Hyperf\Contract\StdoutLoggerInterface;
-use Hyperf\Coordinator\Constants;
-use Hyperf\Coordinator\CoordinatorManager;
 use Hyperf\Event\Contract\ListenerInterface;
 use Hyperf\Framework\Event\MainWorkerStart;
-use Psr\Container\ContainerInterface;
 
 class WatchOnBootListener implements ListenerInterface
 {
-    public function __construct(private ContainerInterface $container, private ConfigInterface $config, private Confd $confd, private StdoutLoggerInterface $logger)
+    public function __construct(private Confd $confd, private StdoutLoggerInterface $logger)
     {
     }
 
@@ -36,13 +32,5 @@ class WatchOnBootListener implements ListenerInterface
     {
         $this->confd->watch();
         $this->logger->debug('[confd] Start watching.');
-
-        while (true) {
-            $isWorkerExited = CoordinatorManager::until(Constants::WORKER_EXIT)->yield(1);
-
-            if ($isWorkerExited) {
-                break;
-            }
-        }
     }
 }
