7fc36c9412c5810f6d2863ac41a81969e4b9b656
Deeka Wong <huangdijia@gmail.com>
2022-09-08 13:00:10 +0800

    Adds command-signals

---
+++ src/command-signals/src/ConfigProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of command-signals.
+ *
+ * @link     https://github.com/friendsofhyperf/command-signals
+ * @document https://github.com/friendsofhyperf/command-signals/blob/main/README.md
+ * @contact  huangdijia@gmail.com
+ */
+namespace FriendsOfHyperf\CommandSignals;
+
+class ConfigProvider
+{
+    public function __invoke()
+    {
+        return [];
+    }
+}
