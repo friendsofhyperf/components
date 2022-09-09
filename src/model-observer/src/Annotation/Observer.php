d60d8c0cc424064574b0bfcaae038fd07cdbffbb
Deeka Wong <huangdijia@gmail.com>
2022-09-09 13:55:21 +0800

    Optimize model observer, Adds multi binding support (#23)
    * Adds multi model bind support
    
    * Update stubs
    
    * Update README.md
    
    * cs-fix

--- src/model-observer/src/Annotation/Observer.php
+++ src/model-observer/src/Annotation/Observer.php
@@ -16,7 +16,10 @@ use Hyperf\Di\Annotation\AbstractAnnotation;
 #[Attribute(Attribute::TARGET_CLASS)]
 class Observer extends AbstractAnnotation
 {
-    public function __construct(public ?string $model = null, public int $priority = 0)
+    /**
+     * @param string|string[] $model
+     */
+    public function __construct(public string|array $model, public int $priority = 0)
     {
     }
 }
