d96189e8e1b5e4f6a7954b73104d3497586f1655
Deeka Wong <huangdijia@gmail.com>
2022-09-13 08:35:53 +0800

    Optimize phpdoc

--- src/http-client/src/PendingRequest.php
+++ src/http-client/src/PendingRequest.php
@@ -84,14 +84,14 @@ class PendingRequest
     /**
      * The request cookies.
      *
-     * @var array
+     * @var \GuzzleHttp\Cookie\CookieJar
      */
     protected $cookies;
 
     /**
      * The transfer stats for the request.
      *
-     * \GuzzleHttp\TransferStats
+     * @var \GuzzleHttp\TransferStats|null
      */
     protected $transferStats;
 
