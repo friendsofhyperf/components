500c9d5f41692b2c065c1a9668fc91dbece77571
Deeka Wong <huangdijia@gmail.com>
2022-09-16 10:52:08 +0800

    Update README.md

--- bin/gen-markdown-table.sh
+++ bin/gen-markdown-table.sh
@@ -3,9 +3,9 @@
 BASEPATH=$(cd `dirname $0`; cd ../src/; pwd)
 REPOS=$(ls $BASEPATH)
 
-echo "|Repository|Stable Version|Unstable Version|Downloads|"
+echo "|Repository|Stable Version|Latest Version|Downloads|"
 echo "|--|--|--|--|"
 
 for REPO in ${REPOS}; do
-    echo "|[${REPO}](https://github.com/friendsofhyperf/${REPO})|[![Latest Stable Version](https://poser.pugx.org/friendsofhyperf/${REPO}/v/stable.svg)](https://packagist.org/packages/friendsofhyperf/${REPO})|[![Latest Unstable Version](https://poser.pugx.org/friendsofhyperf/components/v/unstable.svg)](https://packagist.org/packages/friendsofhyperf/${REPO})|[![Total Downloads](https://img.shields.io/packagist/dt/friendsofhyperf/${REPO})](https://packagist.org/packages/friendsofhyperf/${REPO})|"
+    echo "|[${REPO}](https://github.com/friendsofhyperf/${REPO})|[![Latest Stable Version](https://poser.pugx.org/friendsofhyperf/${REPO}/v/stable.svg)](https://packagist.org/packages/friendsofhyperf/${REPO})|[![Latest Stable Version](https://img.shields.io/packagist/v/friendsofhyperf/${REPO})](https://packagist.org/packages/friendsofhyperf/${REPO})|[![Total Downloads](https://img.shields.io/packagist/dt/friendsofhyperf/${REPO})](https://packagist.org/packages/friendsofhyperf/${REPO})|"
 done
