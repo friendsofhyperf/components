ca84617a1564cb8e160d24124b2dd62ab0ab71d5
Deeka Wong <huangdijia@gmail.com>
2022-09-16 11:28:34 +0800

    Update README.md

--- bin/gen-markdown-table.sh
+++ bin/gen-markdown-table.sh
@@ -3,9 +3,63 @@
 BASEPATH=$(cd `dirname $0`; cd ../src/; pwd)
 REPOS=$(ls $BASEPATH)
 
-echo "|Repository|Stable Version|Latest Version|Downloads|"
-echo "|--|--|--|--|"
+# https://poser.pugx.org/show/friendsofhyperf/components
+
+function github_actions() {
+    echo "[![Latest Test](https://github.com/friendsofhyperf/$1/workflows/tests/badge.svg)](https://github.com/friendsofhyperf/$1/actions)"
+}
+
+function repository() {
+    echo "[$1](https://github.com/friendsofhyperf/$1)"
+}
+
+function latest_stable_version() {
+    echo "[![Latest Stable Version](https://poser.pugx.org/friendsofhyperf/$1/v)](https://packagist.org/packages/friendsofhyperf/$1)"
+}
+
+function latest_unstable_version() {
+    echo "[![Latest Unstable Version](https://poser.pugx.org/friendsofhyperf/$1/v/unstable)](https://packagist.org/packages/friendsofhyperf/$1)"
+}
+
+function total_downloads() {
+    echo "[![Total Downloads](https://poser.pugx.org/friendsofhyperf/$1/downloads)](https://packagist.org/packages/friendsofhyperf/$1)"
+}
+
+function monthly_downloads() {
+    echo "[![Monthly Downloads](https://poser.pugx.org/friendsofhyperf/$1/d/monthly)](https://packagist.org/packages/friendsofhyperf/$1)"
+}
+
+function daily_downloads() {
+    echo "[![Daily Downloads](https://poser.pugx.org/friendsofhyperf/$1/d/daily)](https://packagist.org/packages/friendsofhyperf/$1)"
+}
+
+function php_version_require() {
+    echo "[![PHP Version Require](https://poser.pugx.org/friendsofhyperf/$1/require/php)](https://packagist.org/packages/friendsofhyperf/$1)"
+}
+
+echo "# friendsofhyperf/components"
+echo 
+printf "%s %s %s %s %s %s\n" \
+    "$(github_actions components)" \
+    "$(latest_stable_version components)" \
+    "$(total_downloads components)" \
+    "$(latest_unstable_version components)" \
+    "$(monthly_downloads components)" \
+    "$(php_version_require components)"
+echo
+echo "The most popular components for Hyperf."
+echo 
+echo "## Components"
+echo
+echo "|Repository|Stable Version|Latest Unstable Version|Total Downloads|Monthly Downloads|Daily Downloads|"
+echo "|--|--|--|--|--|--|"
 
 for REPO in ${REPOS}; do
-    echo "|[${REPO}](https://github.com/friendsofhyperf/${REPO})|[![Latest Stable Version](https://poser.pugx.org/friendsofhyperf/${REPO}/v/stable.svg)](https://packagist.org/packages/friendsofhyperf/${REPO})|[![Latest Stable Version](https://img.shields.io/packagist/v/friendsofhyperf/${REPO})](https://packagist.org/packages/friendsofhyperf/${REPO})|[![Total Downloads](https://img.shields.io/packagist/dt/friendsofhyperf/${REPO})](https://packagist.org/packages/friendsofhyperf/${REPO})|"
+    printf "|%s|%s|%s|%s|%s|%s|\n" \
+        "$(repository ${REPO})" \
+        "$(latest_stable_version ${REPO})" \
+        "$(latest_unstable_version ${REPO})" \
+        "$(total_downloads ${REPO})" \
+        "$(monthly_downloads ${REPO})" \
+        "$(daily_downloads ${REPO})"
 done
