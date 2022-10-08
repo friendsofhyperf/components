199ee0cb0a0a467ac97108f14b52c916d8011098
Deeka Wong <huangdijia@gmail.com>
2022-10-08 12:01:15 +0800

    Adds macros for Arr

---
+++ src/macros/src/Arr/Join.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of friendsofhyperf/components.
+ *
+ * @link     https://github.com/friendsofhyperf/components
+ * @document https://github.com/friendsofhyperf/components/blob/3.x/README.md
+ * @contact  huangdijia@gmail.com
+ */
+namespace FriendsOfHyperf\Macros\Arr;
+
+class Join
+{
+    public function __invoke()
+    {
+        return static function ($array, $glue, $finalGlue = '') {
+            if ($finalGlue === '') {
+                return implode($glue, $array);
+            }
+
+            if (count($array) === 0) {
+                return '';
+            }
+
+            if (count($array) === 1) {
+                return end($array);
+            }
+
+            $finalItem = array_pop($array);
+
+            return implode($glue, $array) . $finalGlue . $finalItem;
+        };
+    }
+}
---
+++ src/macros/src/Arr/PrependKeysWith.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of friendsofhyperf/components.
+ *
+ * @link     https://github.com/friendsofhyperf/components
+ * @document https://github.com/friendsofhyperf/components/blob/3.x/README.md
+ * @contact  huangdijia@gmail.com
+ */
+namespace FriendsOfHyperf\Macros\Arr;
+
+use Hyperf\Utils\Collection;
+
+class PrependKeysWith
+{
+    public function __invoke()
+    {
+        return static fn ($array, $prependWith) => Collection::make($array)
+            ->mapWithKeys(function ($item, $key) use ($prependWith) {
+                return [$prependWith . $key => $item];
+            })->all();
+    }
+}
