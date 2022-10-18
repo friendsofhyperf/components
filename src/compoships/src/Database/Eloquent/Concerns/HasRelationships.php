6e6668ec9a37db9b538530ffcd1a1ff4c2edbfdd
Deeka Wong <huangdijia@gmail.com>
2022-10-18 09:51:09 +0800

    cs fix

--- src/compoships/src/Database/Eloquent/Concerns/HasRelationships.php
+++ src/compoships/src/Database/Eloquent/Concerns/HasRelationships.php
@@ -213,8 +213,6 @@ trait HasRelationships
     /**
      * Validate the related model for Compoships compatibility.
      *
-     * @param $related
-     *
      * @throws InvalidUsageException
      */
     private function validateRelatedModel($related)
