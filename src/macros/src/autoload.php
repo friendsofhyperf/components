9f8fdd3a7cf619c008c4d61e0cb89db78ebd7567
huangdijia <huangdijia@gmail.com>
2022-10-23 10:43:02 +0800

    Use array_is_list to determine if it is list

--- src/macros/src/autoload.php
+++ src/macros/src/autoload.php
@@ -19,6 +19,29 @@ use Hyperf\Utils\Collection;
 use Hyperf\Utils\Str;
 use Hyperf\Utils\Stringable;
 
+if (! function_exists('array_is_list')) {
+    /**
+     * Determine if the given value is a list of items.
+     * @return bool return true if the array keys are 0 .. count($array)-1 in that order. For other arrays, it returns false. For non-arrays, it throws a TypeError.
+     */
+    function array_is_list(array $array): bool
+    {
+        if ($array === [] || $array === array_values($array)) {
+            return true;
+        }
+
+        $nextKey = -1;
+
+        foreach ($array as $k => $v) {
+            if ($k !== ++$nextKey) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
 Arr::mixin(new ArrMacros());
 Collection::mixin(new CollectionMacros());
 Request::mixin(new RequestMacros());
