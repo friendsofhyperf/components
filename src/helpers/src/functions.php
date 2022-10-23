c41004e9fc4f3d975252efb4656560eeed0eff72
Deeka Wong <huangdijia@gmail.com>
2022-10-23 10:49:18 +0800

    Optimize `Arr::isList()` and adds `array_is_list()` helper function (#57)
    * Use array_is_list to determine if it is list
    
    * Adds `array_is_list()` to helpers
    
    * cs fix

--- src/helpers/src/functions.php
+++ src/helpers/src/functions.php
@@ -24,6 +24,29 @@ if (! function_exists('app')) {
     }
 }
 
+if (! function_exists('array_is_list')) {
+    /**
+     * Determine if the given value is a list of items.
+     * @return bool return true if the array keys are 0 .. count($array)-1 in that order. For other arrays, it returns false. For non-arrays, it throws a TypeError.
+     */
+    function array_is_list(array $array): bool
+    {
+        if ($array === [] || $array === array_values($array)) {
+            return true;
+        }
+
+        $nextKey = -1;
+
+        foreach ($array as $k => $v) {
+            if ($k !== ++$nextKey) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
 if (! function_exists('blank')) {
     /**
      * Determine if the given value is "blank".
--- src/macros/src/ArrMacros.php
+++ src/macros/src/ArrMacros.php
@@ -20,7 +20,7 @@ class ArrMacros
 {
     public function isList()
     {
-        return fn ($array) => ! Arr::isAssoc($array);
+        return fn ($array) => array_is_list($array);
     }
 
     public function join()
