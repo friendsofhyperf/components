47ecd75a3eb5213d5d89e9f2ae23f82911370080
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2022-11-29 17:19:10 +0800

    format

--- src/sentry/class_map/SentrySdk.php
+++ src/sentry/class_map/SentrySdk.php
@@ -2,10 +2,10 @@
 
 declare(strict_types=1);
 /**
- * This file is part of friendsofhyperf/sentry.
+ * This file is part of friendsofhyperf/components.
  *
- * @link     https://github.com/friendsofhyperf/sentry
- * @document https://github.com/friendsofhyperf/sentry/blob/main/README.md
+ * @link     https://github.com/friendsofhyperf/components
+ * @document https://github.com/friendsofhyperf/components/blob/3.x/README.md
  * @contact  huangdijia@gmail.com
  */
 namespace Sentry;
