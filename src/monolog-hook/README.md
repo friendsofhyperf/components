83fd39040e2c792e07512af48113e6d43498bd65
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2022-12-02 13:08:20 +0800

    Adds monolog hook

---
+++ src/monolog-hook/README.md
@@ -0,0 +1,13 @@
+# Monolog HOOK
+
+[![Latest Stable Version](https://poser.pugx.org/friendsofhyperf/monolog-hook/version.png)](https://packagist.org/packages/friendsofhyperf/monolog-hook)
+[![Total Downloads](https://poser.pugx.org/friendsofhyperf/monolog-hook/d/total.png)](https://packagist.org/packages/friendsofhyperf/monolog-hook)
+[![GitHub license](https://img.shields.io/github/license/friendsofhyperf/monolog-hook)](https://github.com/friendsofhyperf/monolog-hook)
+
+Hyperf HOOK for Monolog, fix SyslogUdpHandler.
+
+## Installation
+
+~~~bash
+composer require friendsofhyperf/monolog-hook
+~~~
---
+++ src/monolog-hook/src/Aspect/UdpSocketAspect.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of friendsofhyperf/components.
+ *
+ * @link     https://github.com/friendsofhyperf/components
+ * @document https://github.com/friendsofhyperf/components/blob/3.x/README.md
+ * @contact  huangdijia@gmail.com
+ */
+namespace FriendsOfHyperf\MonologHook\Aspect;
+
+use Hyperf\Di\Aop\AbstractAspect;
+use Hyperf\Di\Aop\ProceedingJoinPoint;
+use Hyperf\Utils\Coroutine;
+use Monolog\Handler\SyslogUdp\UdpSocket;
+use Swoole\Coroutine\Client;
+
+class UdpSocketAspect extends AbstractAspect
+{
+    public array $classes = [
+        UdpSocket::class . '::send',
+        UdpSocket::class . '::close',
+    ];
+
+    public function process(ProceedingJoinPoint $proceedingJoinPoint)
+    {
+        if (Coroutine::inCoroutine()) {
+            if ($proceedingJoinPoint->methodName == 'close') {
+                return;
+            }
+
+            /** @var string $chunk */
+            $chunk = $proceedingJoinPoint->arguments['keys']['chunk'] ?? '';
+            [$ip, $port] = (fn () => [$this->ip, $this->port])->call($proceedingJoinPoint->getInstance());
+
+            $socket = new Client(SWOOLE_SOCK_UDP);
+            $socket->connect($ip, $port, 0.5);
+
+            defer(function () use ($socket) {
+                $socket->close();
+            });
+
+            $socket->send($chunk);
+
+            return;
+        }
+
+        return $proceedingJoinPoint->process();
+    }
+}
