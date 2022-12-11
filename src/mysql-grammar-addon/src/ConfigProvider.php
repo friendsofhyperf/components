8b8b1524a12f466ab3b8b132b89bf5f8de505b25
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2022-12-12 07:35:10 +0800

    Adds MySql Grammar Addon

---
+++ src/mysql-grammar-addon/src/ConfigProvider.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of friendsofhyperf/components.
+ *
+ * @link     https://github.com/friendsofhyperf/components
+ * @document https://github.com/friendsofhyperf/components/blob/3.x/README.md
+ * @contact  huangdijia@gmail.com
+ */
+namespace FriendsOfHyperf\MySqlGrammarAddon;
+
+use FriendsOfHyperf\MySqlGrammarAddon\Aspect\MySqlGrammarAspect;
+
+class ConfigProvider
+{
+    public function __invoke(): array
+    {
+        return [
+            'aspects' => [
+                MySqlGrammarAspect::class,
+            ],
+        ];
+    }
+}
