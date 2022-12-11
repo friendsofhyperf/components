1a7664e8e8657d3818f39637cd64a58b484f3945
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2022-12-12 07:37:38 +0800

    Adds model-morph-addon

---
+++ src/model-morph-addon/src/ConfigProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of friendsofhyperf/components.
+ *
+ * @link     https://github.com/friendsofhyperf/components
+ * @document https://github.com/friendsofhyperf/components/blob/3.x/README.md
+ * @contact  huangdijia@gmail.com
+ */
+namespace FriendsOfHyperf\ModelMorphAddon;
+
+use FriendsOfHyperf\ModelMorphAddon\Aspect\MorphToAspect;
+use FriendsOfHyperf\ModelMorphAddon\Aspect\QueriesRelationshipsAspect;
+
+class ConfigProvider
+{
+    public function __invoke(): array
+    {
+        return [
+            'aspects' => [
+                MorphToAspect::class,
+                QueriesRelationshipsAspect::class,
+            ],
+        ];
+    }
+}
