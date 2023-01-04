c0a017d87a9e643a82818677d9da520bc34624f6
Deeka Wong <huangdijia@gmail.com>
2023-01-04 17:03:42 +0800

    Adds once (#104)
    Co-authored-by: Deeka Wong <8337659+huangdijia@users.noreply.github.com>

---
+++ src/once/src/ConfigProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of friendsofhyperf/components.
+ *
+ * @link     https://github.com/friendsofhyperf/components
+ * @document https://github.com/friendsofhyperf/components/blob/3.x/README.md
+ * @contact  huangdijia@gmail.com
+ */
+namespace FriendsOfHyperf\Once;
+
+class ConfigProvider
+{
+    public function __invoke(): array
+    {
+        defined('BASE_PATH') or define('BASE_PATH', '');
+
+        return [
+            'aspects' => [
+                Aspect\ForgetAspect::class,
+                Aspect\OnceAspect::class,
+            ],
+        ];
+    }
+}
