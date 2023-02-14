6f26d1bd2b2fd478d17045c3ede6d02473b8e07c
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2023-02-14 12:53:06 +0800

    Adds fast-paginate

---
+++ src/fast-paginate/src/BuilderMixin.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of friendsofhyperf/components.
+ *
+ * @link     https://github.com/friendsofhyperf/components
+ * @document https://github.com/friendsofhyperf/components/blob/3.x/README.md
+ * @contact  huangdijia@gmail.com
+ */
+namespace FriendsOfHyperf\FastPaginate;
+
+class BuilderMixin
+{
+    public function simpleFastPaginate()
+    {
+        return (new FastPaginate())->simpleFastPaginate();
+    }
+
+    public function fastPaginate()
+    {
+        return (new FastPaginate())->fastPaginate();
+    }
+}
---
+++ src/fast-paginate/src/ScoutMixin.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of friendsofhyperf/components.
+ *
+ * @link     https://github.com/friendsofhyperf/components
+ * @document https://github.com/friendsofhyperf/components/blob/3.x/README.md
+ * @contact  huangdijia@gmail.com
+ */
+namespace FriendsOfHyperf\FastPaginate;
+
+/**
+ * @mixin \Hyperf\Scout\Builder
+ */
+class ScoutMixin
+{
+    public function fastPaginate($perPage = null, $pageName = 'page', $page = null)
+    {
+        // Just defer to the Scout Builder for DX purposes.
+        return fn ($perPage = null, $pageName = 'page', $page = null) => $this->paginate($perPage, $pageName, $page);
+    }
+}
