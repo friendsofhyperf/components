becb8e5503c3df2a7d49e67944d4db2463bbb250
何泽宏 <hzh@addcn.com>
2023-03-13 13:25:03 +0800

    Optimize: sentry guzzle aspect

--- src/sentry/src/Aspect/GuzzleHttpClientAspect.php
+++ src/sentry/src/Aspect/GuzzleHttpClientAspect.php
@@ -57,9 +57,9 @@ class GuzzleHttpClientAspect extends AbstractAspect
             }
 
             $uri = $arguments['keys']['uri'] ?? '';
-            $data['request']['uri'] = $guzzleConfig['base_uri'] . $uri;
-            $data['request']['method'] = $arguments['keys']['method'] ?? 'GET';
-            $data['request']['options'] = $arguments['keys']['options'] ?? [];
+            $data['uri'] = $guzzleConfig['base_uri'] . $uri;
+            $data['method'] = $arguments['keys']['method'] ?? 'GET';
+            $data['options'] = $arguments['keys']['options'] ?? [];
             $data['timeMs'] = (microtime(true) - $startTime) * 1000;
 
             Integration::addBreadcrumb(new Breadcrumb(
