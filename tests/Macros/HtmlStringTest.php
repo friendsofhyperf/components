79b7b6d621455f970e63c55e3cf5a76b11ac5b38
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2023-03-20 13:59:25 +0800

    Trans to pest

--- tests/Macros/HtmlStringTest.php
+++ tests/Macros/HtmlStringTest.php
@@ -8,38 +8,24 @@ declare(strict_types=1);
  * @document https://github.com/friendsofhyperf/components/blob/3.x/README.md
  * @contact  huangdijia@gmail.com
  */
-namespace FriendsOfHyperf\Tests\Macros;
-
 use FriendsOfHyperf\Macros\Foundation\HtmlString;
-use FriendsOfHyperf\Tests\TestCase;
 
-/**
- * @internal
- * @coversNothing
- */
-class HtmlStringTest extends TestCase
-{
-    public function testToHtml()
-    {
-        $str = '<h1>foo</h1>';
-        $html = new HtmlString('<h1>foo</h1>');
-        $this->assertEquals($str, $html->toHtml());
-    }
+test('test ToHtml', function () {
+    $str = '<h1>foo</h1>';
+    $html = new HtmlString('<h1>foo</h1>');
+    $this->assertEquals($str, $html->toHtml());
+});
 
-    public function testToString()
-    {
-        $str = '<h1>foo</h1>';
-        $html = new HtmlString('<h1>foo</h1>');
-        $this->assertEquals($str, (string) $html);
-    }
+test('test ToString', function () {
+    $str = '<h1>foo</h1>';
+    $html = new HtmlString('<h1>foo</h1>');
+    $this->assertEquals($str, (string) $html);
+});
 
-    public function testIsEmpty()
-    {
-        $this->assertTrue((new HtmlString(''))->isEmpty());
-    }
+test('test IsEmpty', function () {
+    $this->assertTrue((new HtmlString(''))->isEmpty());
+});
 
-    public function testIsNotEmpty()
-    {
-        $this->assertTrue((new HtmlString('foo'))->isNotEmpty());
-    }
-}
+test('test IsNotEmpty', function () {
+    $this->assertTrue((new HtmlString('foo'))->isNotEmpty());
+});
