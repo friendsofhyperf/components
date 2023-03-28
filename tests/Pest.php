776559cb3d9a8664f9277497e63c2384d6bac451
huangdijia <huangdijia@gmail.com>
2023-03-28 20:58:04 +0800

    ⚙️chore(tests) add strict_types declaration and update file header

--- tests/Pest.php
+++ tests/Pest.php
@@ -1,16 +1,13 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Test Case
-|--------------------------------------------------------------------------
-|
-| The closure you provide to your test functions is always bound to a specific PHPUnit test
-| case class. By default, that class is "PHPUnit\Framework\TestCase". Of course, you may
-| need to change it using the "uses()" function to bind a different classes or traits.
-|
-*/
-
+declare(strict_types=1);
+/**
+ * This file is part of friendsofhyperf/components.
+ *
+ * @link     https://github.com/friendsofhyperf/components
+ * @document https://github.com/friendsofhyperf/components/blob/3.x/README.md
+ * @contact  huangdijia@gmail.com
+ */
 // uses(Tests\TestCase::class)->in('Feature');
 
 /*
