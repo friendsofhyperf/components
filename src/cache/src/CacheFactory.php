60ac7007c634e0f8bf97fb4c8b4536c0aad1d761
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2023-04-06 08:13:37 +0800

    ⚙️ chore(cache): remove unused exception imports in CacheFactory.php

--- src/cache/src/CacheFactory.php
+++ src/cache/src/CacheFactory.php
@@ -10,17 +10,10 @@ declare(strict_types=1);
  */
 namespace FriendsOfHyperf\Cache;
 
-use Hyperf\Cache\Exception\InvalidArgumentException;
 use Psr\Container\ContainerInterface;
-use TypeError;
 
 class CacheFactory
 {
-    /**
-     * @return CacheInterface
-     * @throws TypeError
-     * @throws InvalidArgumentException
-     */
     public function __invoke(ContainerInterface $container)
     {
         return $container->get(CacheManager::class)->store('default');
