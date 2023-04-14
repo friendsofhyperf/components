3b1acc7ac504f97eb08df7961d0c9337271495e9
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2023-04-14 14:05:49 +0800

    Compatible with `Hyperf\Utils\Stringable`

--- src/tinker/src/Command/TinkerCommand.php
+++ src/tinker/src/Command/TinkerCommand.php
@@ -42,6 +42,7 @@ class TinkerCommand extends HyperfCommand
         'Hyperf\Redis\Redis' => 'FriendsOfHyperf\Tinker\TinkerCaster::castRedis',
         'Hyperf\Utils\Collection' => 'FriendsOfHyperf\Tinker\TinkerCaster::castCollection',
         'Hyperf\Collection\Collection' => 'FriendsOfHyperf\Tinker\TinkerCaster::castCollection',
+        'Hyperf\Utils\Stringable' => 'FriendsOfHyperf\Tinker\TinkerCaster::castStringable',
         'Hyperf\Stringable\Stringable' => 'FriendsOfHyperf\Tinker\TinkerCaster::castStringable',
         'Hyperf\ViewEngine\HtmlString' => 'FriendsOfHyperf\Tinker\TinkerCaster::castHtmlString',
         'Symfony\Component\Console\Application' => 'FriendsOfHyperf\Tinker\TinkerCaster::castApplication',
