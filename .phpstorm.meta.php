2479171c3df65bd48aacefa52746c458e499356f
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2023-04-26 15:19:49 +0800

    ⚙️chore(PHPStorm) update PHPStorm meta data mappings for new dependencies

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -11,14 +11,17 @@ declare(strict_types=1);
 namespace PHPSTORM_META;
 
 // Reflect
-override(\Psr\Container\ContainerInterface::get(0), map(['' => '@']));
-override(\Hyperf\Context\Context::get(0), map(['' => '@']));
 override(\app(0), map(['' => '@']));
 override(\di(0), map(['' => '@']));
 override(\resolve(0), map(['' => '@']));
 override(\make(0), map(['' => '@']));
-override(\Hyperf\Support\make(0), map(['' => '@']));
 override(\optional(0), type(0));
-override(\Hyperf\Support\optional(0), type(0));
 override(\tap(0), type(0));
+
+override(\FriendsOfHyperf\Helpers\app(0), map(['' => '@']));
+override(\FriendsOfHyperf\Helpers\di(0), map(['' => '@']));
+override(\Hyperf\Context\Context::get(0), map(['' => '@']));
+override(\Hyperf\Support\make(0), map(['' => '@']));
+override(\Hyperf\Support\optional(0), type(0));
 override(\Hyperf\Tappable\tap(0), type(0));
+override(\Psr\Container\ContainerInterface::get(0), map(['' => '@']));
