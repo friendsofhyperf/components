ec20d21a476f1e0e6e5e0d933f45212a5a30c40b
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2023-05-12 12:10:58 +0800

    ⚙️ chore(listener): add new request lifecycle listener

---
+++ src/sentry/src/Listener/CheckIsEnableRequestLifecycleListener.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of friendsofhyperf/components.
+ *
+ * @link     https://github.com/friendsofhyperf/components
+ * @document https://github.com/friendsofhyperf/components/blob/3.x/README.md
+ * @contact  huangdijia@gmail.com
+ */
+namespace FriendsOfHyperf\Sentry\Listener;
+
+use Hyperf\Contract\StdoutLoggerInterface;
+use Hyperf\Event\Contract\ListenerInterface;
+use Hyperf\Framework\Event\BootApplication;
+use Hyperf\Server\Port;
+use Hyperf\Server\ServerFactory;
+use Psr\Container\ContainerInterface;
+
+class CheckIsEnableRequestLifecycleListener implements ListenerInterface
+{
+    public function __construct(protected ContainerInterface $container)
+    {
+    }
+
+    public function listen(): array
+    {
+        return [
+            BootApplication::class,
+        ];
+    }
+
+    public function process(object $event): void
+    {
+        /** @var Port[] $ports */
+        $ports = $this->container->get(ServerFactory::class)->getConfig()?->getServers();
+
+        if (! $ports) {
+            return;
+        }
+
+        foreach ($ports as $port) {
+            if ($port->getOptions()?->isEnableRequestLifecycle()) {
+                return;
+            }
+        }
+
+        $this->container->get(StdoutLoggerInterface::class)->warning('Sentry: Request lifecycle is not enabled, please set `enable_request_lifecycle` to true in server config.');
+    }
+}
