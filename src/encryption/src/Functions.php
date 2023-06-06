e2f231e0e87f413b977206cb8973c078a0dc8313
Deeka Wong <huangdijia@gmail.com>
2023-06-06 12:24:30 +0800

    Remove deprecated codes (#225)
    * 🛠️refactor(autoload): remove async-task Functions.php from composer.json
    
    * ⚙️chore(cache) remove deprecated method `CacheManager::get()`
    
    * 🛠️refactor(encryption) refactor encrypt and decrypt functions
    
    * Remove deprecated functions
    
    * ⚙️chore(pipeline) delete Hub.php file
    
    * 🛠️refactor(helpers) remove deprecated functions from autoload in composer.json
    
    * ⚙️chore(dependencies) remove unused http-client functions in composer.json
    
    * ⚙️chore(dependencies) remove unused files
    
    * Remove deprecated functions
    
    * Format
    
    * ⚙️chore(dependencies) update composer/composer to v2.0 in composer.json
    
    ---------
    
    Co-authored-by: Deeka Wong <8337659+huangdijia@users.noreply.github.com>

--- src/encryption/src/Functions.php
+++ src/encryption/src/Functions.php
@@ -8,58 +8,30 @@ declare(strict_types=1);
  * @document https://github.com/friendsofhyperf/components/blob/3.x/README.md
  * @contact  huangdijia@gmail.com
  */
-namespace Friendsofhyperf\Encryption{
-    use Hyperf\Context\ApplicationContext;
+namespace Friendsofhyperf\Encryption;
 
-    /**
-     * Decrypt the given value.
-     *
-     * @return mixed
-     */
-    function decrypt(string $value, bool $unserialize = true)
-    {
-        return ApplicationContext::getContainer()
-            ->get(Encrypter::class)
-            ->decrypt($value, $unserialize);
-    }
+use Hyperf\Context\ApplicationContext;
 
-    /**
-     * Encrypt the given value.
-     *
-     * @param mixed $value
-     */
-    function encrypt($value, bool $serialize = true): string
-    {
-        return ApplicationContext::getContainer()
-            ->get(Encrypter::class)
-            ->encrypt($value, $serialize);
-    }
+/**
+ * Decrypt the given value.
+ *
+ * @return mixed
+ */
+function decrypt(string $value, bool $unserialize = true)
+{
+    return ApplicationContext::getContainer()
+        ->get(Encrypter::class)
+        ->decrypt($value, $unserialize);
 }
 
-namespace {
-    if (! function_exists('decrypt')) {
-        /**
-         * Decrypt the given value.
-         *
-         * @return mixed
-         * @deprecated since 3.1, please use `\Friendsofhyperf\Encryption\decrypt` instead.
-         */
-        function decrypt(string $value, bool $unserialize = true)
-        {
-            return \Friendsofhyperf\Encryption\decrypt($value, $unserialize);
-        }
-    }
-
-    if (! function_exists('encrypt')) {
-        /**
-         * Encrypt the given value.
-         *
-         * @param mixed $value
-         * @deprecated since 3.1, please use `\Friendsofhyperf\Encryption\encrypt` instead.
-         */
-        function encrypt($value, bool $serialize = true): string
-        {
-            return \Friendsofhyperf\Encryption\encrypt($value, $serialize);
-        }
-    }
+/**
+ * Encrypt the given value.
+ *
+ * @param mixed $value
+ */
+function encrypt($value, bool $serialize = true): string
+{
+    return ApplicationContext::getContainer()
+        ->get(Encrypter::class)
+        ->encrypt($value, $serialize);
 }
