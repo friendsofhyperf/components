0cf456f4399ccb8ded45790841dd3b97fdf3c0f0
Deeka Wong <huangdijia@gmail.com>
2023-06-12 07:52:19 +0800

    [3.0]Adds `gateway-worker` component (#235)
    * Adds `gateway-worker` component
    
    * ⚙️chore(dependencies) Update capitalization and remove unnecessary dependencies in tests.yaml
    
    * Optimize CI
    
    ---------
    
    Co-authored-by: Deeka Wong <8337659+huangdijia@users.noreply.github.com>

---
+++ src/gateway-worker/src/Client.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of friendsofhyperf/components.
+ *
+ * @link     https://github.com/friendsofhyperf/components
+ * @document https://github.com/friendsofhyperf/components/blob/3.x/README.md
+ * @contact  huangdijia@gmail.com
+ */
+namespace FriendsOfHyperf\GatewayWorker;
+
+use GatewayClient\Gateway;
+use Hyperf\Macroable\Macroable;
+
+class Client extends Gateway
+{
+    use Macroable;
+}
