66baa3f0d31d0cc45ef453df85c52cd6b624729f
Deeka Wong <huangdijia@gmail.com>
2023-06-12 13:10:10 +0800

    Adds `openai-client` (#237)
    * Adds `openai-client` component
    
    * Optimize CI
    
    ---------
    
    Co-authored-by: Deeka Wong <8337659+huangdijia@users.noreply.github.com>

---
+++ src/openai-client/src/Exception/ApiKeyIsMissing.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of friendsofhyperf/components.
+ *
+ * @link     https://github.com/friendsofhyperf/components
+ * @document https://github.com/friendsofhyperf/components/blob/3.x/README.md
+ * @contact  huangdijia@gmail.com
+ */
+namespace FriendsOfHyperf\OpenAi\Exception;
+
+use InvalidArgumentException;
+
+/**
+ * @internal
+ */
+final class ApiKeyIsMissing extends InvalidArgumentException
+{
+    /**
+     * Create a new exception instance.
+     */
+    public static function create(): self
+    {
+        return new self(
+            'The OpenAI API Key is missing. Please publish the [openai.php] configuration file and set the [api_key].'
+        );
+    }
+}
