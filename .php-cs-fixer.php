3888b3a56a5840ef63fc520e80d20e8b2423ee73
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2023-06-16 15:23:43 +0800

    Cs Fix

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -5,14 +5,14 @@ declare(strict_types=1);
  * This file is part of friendsofhyperf/components.
  *
  * @link     https://github.com/friendsofhyperf/components
- * @document https://github.com/friendsofhyperf/components/blob/3.x/README.md
+ * @document https://github.com/friendsofhyperf/components/blob/3.0/README.md
  * @contact  huangdijia@gmail.com
  */
 $header = <<<'EOF'
 This file is part of friendsofhyperf/components.
 
 @link     https://github.com/friendsofhyperf/components
-@document https://github.com/friendsofhyperf/components/blob/3.x/README.md
+@document https://github.com/friendsofhyperf/components/blob/3.0/README.md
 @contact  huangdijia@gmail.com
 EOF;
 
--- src/http-request-lifecycle/src/Events/AbstractEvent.php
+++ src/http-request-lifecycle/src/Events/AbstractEvent.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
  * This file is part of friendsofhyperf/components.
  *
  * @link     https://github.com/friendsofhyperf/components
- * @document https://github.com/friendsofhyperf/components/blob/3.x/README.md
+ * @document https://github.com/friendsofhyperf/components/blob/3.0/README.md
  * @contact  huangdijia@gmail.com
  */
 namespace FriendsOfHyperf\Http\RequestLifeCycle\Events;
