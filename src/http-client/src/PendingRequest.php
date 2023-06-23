724c085fc1620b48f22f0a0d8ad3405615b7e48b
Deeka Wong <huangdijia@gmail.com>
2023-06-23 09:26:48 +0800

    Add withQueryParameters to the HTTP client (#252)

--- src/http-client/src/PendingRequest.php
+++ src/http-client/src/PendingRequest.php
@@ -355,6 +355,20 @@ class PendingRequest
     }
 
     /**
+     * Set the given query parameters in the request URI.
+     *
+     * @return $this
+     */
+    public function withQueryParameters(array $parameters)
+    {
+        return tap($this, function () use ($parameters) {
+            $this->options = array_merge_recursive($this->options, [
+                'query' => $parameters,
+            ]);
+        });
+    }
+
+    /**
      * Specify the request's content type.
      *
      * @return $this
