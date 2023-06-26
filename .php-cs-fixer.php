7c1e623b04d72aa321b33892d7294e0f35e55645
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2023-06-26 13:21:41 +0800

    Using new coding standard

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -8,107 +8,27 @@ declare(strict_types=1);
  * @document https://github.com/friendsofhyperf/components/blob/main/README.md
  * @contact  huangdijia@gmail.com
  */
-$header = <<<'EOF'
-This file is part of friendsofhyperf/components.
+use Huangdijia\PhpCsFixer\Config;
 
-@link     https://github.com/friendsofhyperf/components
-@document https://github.com/friendsofhyperf/components/blob/main/README.md
-@contact  huangdijia@gmail.com
-EOF;
+require __DIR__ . '/vendor/autoload.php';
 
-return (new PhpCsFixer\Config())
-    ->setRiskyAllowed(true)
-    ->setRules([
-        '@PSR2' => true,
-        '@Symfony' => true,
-        '@DoctrineAnnotation' => true,
-        '@PhpCsFixer' => true,
-        'header_comment' => [
-            'comment_type' => 'PHPDoc',
-            'header' => $header,
-            'separate' => 'none',
-            'location' => 'after_declare_strict',
+return (new Config())
+    ->setHeaderComment(
+        projectName: 'friendsofhyperf/components',
+        projectLink: 'https://github.com/friendsofhyperf/components',
+        projectDocument: 'https://github.com/friendsofhyperf/components/blob/main/README.md',
+        contacts: [
+            'huangdijia@gmail.com',
         ],
-        'array_syntax' => [
-            'syntax' => 'short',
-        ],
-        'blank_line_before_statement' => [
-            'statements' => [
-                'declare',
-            ],
-        ],
-        'list_syntax' => [
-            'syntax' => 'short',
-        ],
-        'concat_space' => [
-            'spacing' => 'one',
-        ],
-        'general_phpdoc_annotation_remove' => [
-            'annotations' => [
-                'author',
-            ],
-        ],
-        'global_namespace_import' => [
-            'import_classes' => true,
-            'import_constants' => true,
-            'import_functions' => null,
-        ],
-        'ordered_imports' => [
-            'imports_order' => [
-                'class', 'function', 'const',
-            ],
-            'sort_algorithm' => 'alpha',
-        ],
-        'single_line_comment_style' => [
-            'comment_types' => [
-            ],
-        ],
-        'yoda_style' => [
-            'always_move_variable' => false,
-            'equal' => false,
-            'identical' => false,
-        ],
-        'phpdoc_align' => [
-            'align' => 'left',
-        ],
-        'multiline_whitespace_before_semicolons' => [
-            'strategy' => 'no_multi_line',
-        ],
-        'constant_case' => [
-            'case' => 'lower',
-        ],
-        'class_attributes_separation' => true,
-        'combine_consecutive_unsets' => true,
-        'declare_strict_types' => true,
-        'linebreak_after_opening_tag' => true,
-        'lowercase_static_reference' => true,
-        'no_useless_else' => true,
-        'no_unused_imports' => true,
-        'not_operator_with_successor_space' => true,
-        'not_operator_with_space' => false,
-        'ordered_class_elements' => true,
-        'php_unit_strict' => false,
-        'phpdoc_separation' => false,
-        'single_quote' => true,
-        'standardize_not_equals' => true,
-        'multiline_comment_opening_closing' => true,
-        'php_unit_test_class_requires_covers' => true,
-        'php_unit_internal_class' => true,
-        'ordered_types' => [
-            'null_adjustment' => 'always_last',
-            'sort_algorithm' => 'none',
-        ],
-        'phpdoc_types_order' => [
-            'null_adjustment' => 'always_last',
-            'sort_algorithm' => 'none',
-        ],
-    ])
+    )
     ->setFinder(
         PhpCsFixer\Finder::create()
             ->exclude('bin')
-            ->exclude('public')
             ->exclude('runtime')
             ->exclude('vendor')
             ->in(__DIR__)
+            ->append([
+                __FILE__,
+            ])
     )
     ->setUsingCache(false);
