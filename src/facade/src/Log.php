cc0905d5a9812b1c68705791d969f16627b53f42
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2023-06-27 16:23:28 +0800

    Adds PHPDoc

--- src/facade/src/Log.php
+++ src/facade/src/Log.php
@@ -15,6 +15,15 @@ use Hyperf\Logger\Logger;
 use Hyperf\Logger\LoggerFactory;
 
 /**
+ * @method static void emergency(string|\Stringable $message, array $context = [])
+ * @method static void alert(string|\Stringable $message, array $context = [])
+ * @method static void critical(string|\Stringable $message, array $context = [])
+ * @method static void error(string|\Stringable $message, array $context = [])
+ * @method static void warning(string|\Stringable $message, array $context = [])
+ * @method static void notice(string|\Stringable $message, array $context = [])
+ * @method static void info(string|\Stringable $message, array $context = [])
+ * @method static void debug(string|\Stringable $message, array $context = [])
+ * @method static void log($level, string|\Stringable $message, array $context = [])
  * @mixin LoggerFactory
  * @mixin Logger
  */
