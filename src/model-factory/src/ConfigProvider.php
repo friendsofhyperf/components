d72835cf2b51f63c32cda5d47d20a8d5af12d118
Deeka Wong <huangdijia@gmail.com>
2023-07-02 16:04:00 +0800

    Adds `model-factory` component (#262)
    * Adds `model-factory` component
    
    * Adds dependencies

---
+++ src/model-factory/src/ConfigProvider.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of friendsofhyperf/components.
+ *
+ * @link     https://github.com/friendsofhyperf/components
+ * @document https://github.com/friendsofhyperf/components/blob/3.0/README.md
+ * @contact  huangdijia@gmail.com
+ */
+namespace FriendsOfHyperf\ModelFactory;
+
+use Hyperf\Database\Model\Factory;
+
+class ConfigProvider
+{
+    public function __invoke()
+    {
+        return [
+            'dependencies' => [
+                Factory::class => FactoryInvoker::class,
+            ],
+            'publish' => [
+                [
+                    'id' => 'model_factory',
+                    'description' => 'The config for model factory.',
+                    'source' => __DIR__ . '/../publish/model_factory.php',
+                    'destination' => BASE_PATH . '/config/autoload/model_factory.php',
+                ],
+                [
+                    'id' => 'model_factory_example',
+                    'description' => 'The example for model factory.',
+                    'source' => __DIR__ . '/../publish/example.php.stub',
+                    'destination' => BASE_PATH . '/database/factory/ModelFactory.php',
+                ],
+            ],
+        ];
+    }
+}
