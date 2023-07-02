ef6d0bfe1439c40ef87ce21619d7e4b57d53ab20
Deeka Wong <huangdijia@gmail.com>
2023-07-02 16:04:00 +0800

    Adds `model-factory` component (#262)
    * Adds `model-factory` component
    
    * Adds dependencies

---
+++ src/model-factory/src/FactoryInvoker.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of friendsofhyperf/components.
+ *
+ * @link     https://github.com/friendsofhyperf/components
+ * @document https://github.com/friendsofhyperf/components/blob/3.0/README.md
+ * @contact  huangdijia@gmail.com
+ */
+namespace FriendsOfHyperf\ModelFactory;
+
+use Faker\Factory as FakerFactory;
+use Hyperf\Contract\ConfigInterface;
+use Hyperf\Database\Model\Factory;
+use Psr\Container\ContainerInterface;
+
+class FactoryInvoker
+{
+    public function __invoke(ContainerInterface $container)
+    {
+        $config = $container->get(ConfigInterface::class);
+        $factory = new Factory(
+            FakerFactory::create('en_US')
+        );
+
+        if (is_dir($path = $config->get('model_factory.path', ''))) {
+            $factory->load($path);
+        }
+
+        return $factory;
+    }
+}
