2578817bc8d63ebaeca8bb5bdf43ca0afd670d32
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2023-08-08 07:38:00 +0800

    Optimize the exitCode when exception handled

--- src/confd/src/Command/EnvCommand.php
+++ src/confd/src/Command/EnvCommand.php
@@ -16,6 +16,7 @@ use Hyperf\Command\Command as HyperfCommand;
 use Hyperf\Contract\ConfigInterface;
 use Hyperf\Contract\StdoutLoggerInterface;
 use Psr\Container\ContainerInterface;
+use Throwable;
 
 use function Hyperf\Support\make;
 
@@ -25,8 +26,12 @@ class EnvCommand extends HyperfCommand
 
     protected string $description = 'Upgrade .env by confd.';
 
-    public function __construct(protected ContainerInterface $container, protected ConfigInterface $config, protected StdoutLoggerInterface $logger)
-    {
+    public function __construct(
+        protected ContainerInterface $container,
+        protected ConfigInterface $config,
+        protected StdoutLoggerInterface $logger,
+        protected Confd $confd
+    ) {
         parent::__construct();
     }
 
@@ -35,17 +40,21 @@ class EnvCommand extends HyperfCommand
         $path = (string) ($this->input->getOption('env-path') ?? $this->config->get('confd.env_path'));
 
         if (! is_file($path)) {
-            $this->error($path . ' is not exists!');
-            return;
+            $this->error('The env file "' . $path . '" is not exists!');
+            return $this->exitCode = 1;
         }
 
-        $writer = $this->makeWriter($path);
-        $confd = $this->container->get(Confd::class);
-        $values = $confd->fetch();
+        try {
+            $writer = $this->makeWriter($path);
+            $values = $this->confd->fetch();
 
-        $writer->setValues($values)->write();
+            $writer->setValues($values)->write();
 
-        $this->logger->info($path . ' is updated.');
+            $this->logger->info($path . ' is updated.');
+        } catch (Throwable $e) {
+            $this->logger->error((string) $e);
+            return $this->exitCode = 1;
+        }
     }
 
     public function makeWriter(string $path): EnvWriter
