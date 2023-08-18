350906e0ff0559a11ec66a405755feaf52431407
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2023-08-18 11:55:26 +0800

    Create CoroutineAspect.php

---
+++ src/sentry/src/Aspect/CoroutineAspect.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of friendsofhyperf/components.
+ *
+ * @link     https://github.com/friendsofhyperf/components
+ * @document https://github.com/friendsofhyperf/components/blob/3.0/README.md
+ * @contact  huangdijia@gmail.com
+ */
+
+namespace Hyperf\Tracer\Aspect;
+
+use Hyperf\Context\Context;
+use Hyperf\Di\Aop\AbstractAspect;
+use Hyperf\Di\Aop\ProceedingJoinPoint;
+use Hyperf\Engine\Coroutine as Co;
+use Sentry\SentrySdk;
+
+class CoroutineAspect extends AbstractAspect
+{
+    public array $classes = [
+        'Hyperf\Coroutine\Coroutine::create',
+    ];
+
+    public function process(ProceedingJoinPoint $proceedingJoinPoint)
+    {
+        $callable = $proceedingJoinPoint->arguments['keys']['callable'];
+
+        $proceedingJoinPoint->arguments['keys']['callable'] = function () use ($callable) {
+            Context::copy(Co::pid(), [SentrySdk::class]);
+            $callable();
+        };
+
+        return $proceedingJoinPoint->process();
+    }
+}
