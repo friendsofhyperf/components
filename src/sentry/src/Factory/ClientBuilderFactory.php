9b535db44b622a6c8fc4777834d2c1602ad8897b
Deeka Wong <huangdijia@gmail.com>
2023-08-22 12:28:39 +0800

    Revert ClientBuilderFactory and HubFactory (#314)
    Co-authored-by: Deeka Wong <8337659+huangdijia@users.noreply.github.com>

--- src/sentry/src/Factory/ClientBuilderFactory.php
+++ src/sentry/src/Factory/ClientBuilderFactory.php
@@ -11,20 +11,13 @@ declare(strict_types=1);
 
 namespace FriendsOfHyperf\Sentry\Factory;
 
-use FriendsOfHyperf\Sentry\Integration;
-use FriendsOfHyperf\Sentry\Integration\RequestFetcher;
 use FriendsOfHyperf\Sentry\Version;
-use Hyperf\Context\Context;
 use Hyperf\Contract\ConfigInterface;
 use Hyperf\Contract\StdoutLoggerInterface;
 use Psr\Container\ContainerInterface;
-use Psr\Http\Message\ServerRequestInterface;
-use RuntimeException;
 use Sentry\ClientBuilder;
-use Sentry\Integration as SdkIntegration;
 
 use function Hyperf\Support\env;
-use function Hyperf\Support\make;
 use function Hyperf\Tappable\tap;
 
 class ClientBuilderFactory
@@ -63,79 +56,9 @@ class ClientBuilderFactory
             $options['environment'] = env('APP_ENV', 'production');
         }
 
-        return tap(ClientBuilder::create($options), function (ClientBuilder $clientBuilder) use ($container) {
+        return tap(ClientBuilder::create($options), function (ClientBuilder $clientBuilder) {
             $clientBuilder->setSdkIdentifier(Version::SDK_IDENTIFIER);
             $clientBuilder->setSdkVersion(Version::SDK_VERSION);
-
-            $options = $clientBuilder->getOptions();
-            $userIntegrations = $this->resolveIntegrationsFromUserConfig($container);
-
-            $options->setIntegrations(static function (array $integrations) use ($options, $userIntegrations, $container) {
-                if ($options->hasDefaultIntegrations()) {
-                    // Remove the default error and fatal exception listeners to let Laravel handle those
-                    // itself. These event are still bubbling up through the documented changes in the users
-                    // `ExceptionHandler` of their application or through the log channel integration to Sentry
-                    $integrations = array_filter($integrations, static function (SdkIntegration\IntegrationInterface $integration): bool {
-                        if ($integration instanceof SdkIntegration\ErrorListenerIntegration) {
-                            return false;
-                        }
-
-                        if ($integration instanceof SdkIntegration\ExceptionListenerIntegration) {
-                            return false;
-                        }
-
-                        if ($integration instanceof SdkIntegration\FatalErrorListenerIntegration) {
-                            return false;
-                        }
-
-                        // We also remove the default request integration so it can be readded
-                        // after with a Laravel specific request fetcher. This way we can resolve
-                        // the request from Laravel instead of constructing it from the global state
-                        if ($integration instanceof SdkIntegration\RequestIntegration) {
-                            return false;
-                        }
-
-                        return true;
-                    });
-                }
-
-                $requestFetcher = null;
-
-                if (Context::has(ServerRequestInterface::class)) {
-                    $requestFetcher = $container->get(RequestFetcher::class);
-                }
-
-                $integrations[] = new SdkIntegration\RequestIntegration($requestFetcher);
-
-                return array_merge($integrations, $userIntegrations);
-            });
         });
-    }
-
-    protected function resolveIntegrationsFromUserConfig(ContainerInterface $container): array
-    {
-        $integrations = [
-            new Integration(),
-        ];
-        $config = $container->get(ConfigInterface::class)->get('sentry', []);
-        $userIntegrations = $config['integrations'] ?? [];
-
-        foreach ($userIntegrations as $userIntegration) {
-            if ($userIntegration instanceof SdkIntegration\IntegrationInterface) {
-                $integrations[] = $userIntegration;
-            } elseif (\is_string($userIntegration)) {
-                $resolvedIntegration = make($userIntegration);
-
-                if (! $resolvedIntegration instanceof SdkIntegration\IntegrationInterface) {
-                    throw new RuntimeException('Sentry integrations should a instance of `\Sentry\Integration\IntegrationInterface`.');
-                }
-
-                $integrations[] = $resolvedIntegration;
-            } else {
-                throw new RuntimeException('Sentry integrations should either be a container reference or a instance of `\Sentry\Integration\IntegrationInterface`.');
-            }
-        }
-
-        return $integrations;
     }
 }
--- src/sentry/src/Factory/HubFactory.php
+++ src/sentry/src/Factory/HubFactory.php
@@ -11,8 +11,15 @@ declare(strict_types=1);
 
 namespace FriendsOfHyperf\Sentry\Factory;
 
+use FriendsOfHyperf\Sentry\Integration;
+use FriendsOfHyperf\Sentry\Integration\RequestFetcher;
+use Hyperf\Context\Context;
+use Hyperf\Contract\ConfigInterface;
 use Psr\Container\ContainerInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use RuntimeException;
 use Sentry\ClientBuilderInterface;
+use Sentry\Integration as SdkIntegration;
 use Sentry\State\Hub;
 
 use function Hyperf\Support\make;
@@ -26,11 +33,83 @@ class HubFactory
     public function __invoke(ContainerInterface $container)
     {
         $clientBuilder = $container->get(ClientBuilderInterface::class);
+        $options = $clientBuilder->getOptions();
+        $userIntegrations = $this->resolveIntegrationsFromUserConfig($container);
+
+        $options->setIntegrations(static function (array $integrations) use ($options, $userIntegrations, $container) {
+            if ($options->hasDefaultIntegrations()) {
+                // Remove the default error and fatal exception listeners to let Laravel handle those
+                // itself. These event are still bubbling up through the documented changes in the users
+                // `ExceptionHandler` of their application or through the log channel integration to Sentry
+                $integrations = array_filter($integrations, static function (SdkIntegration\IntegrationInterface $integration): bool {
+                    if ($integration instanceof SdkIntegration\ErrorListenerIntegration) {
+                        return false;
+                    }
+
+                    if ($integration instanceof SdkIntegration\ExceptionListenerIntegration) {
+                        return false;
+                    }
+
+                    if ($integration instanceof SdkIntegration\FatalErrorListenerIntegration) {
+                        return false;
+                    }
+
+                    // We also remove the default request integration so it can be readded
+                    // after with a Laravel specific request fetcher. This way we can resolve
+                    // the request from Laravel instead of constructing it from the global state
+                    if ($integration instanceof SdkIntegration\RequestIntegration) {
+                        return false;
+                    }
+
+                    return true;
+                });
+            }
+
+            $requestFetcher = null;
+
+            if (Context::has(ServerRequestInterface::class)) {
+                $requestFetcher = $container->get(RequestFetcher::class);
+            }
+
+            $integrations[] = new SdkIntegration\RequestIntegration($requestFetcher);
+
+            return array_merge($integrations, $userIntegrations);
+        });
+
         $client = (function () {
             $this->transport = null; // Make the transport is new created before get client
             return $this->getClient();
         })->call($clientBuilder);
 
         return new Hub($client);
+    }
+
+    /**
+     * @return SdkIntegration\IntegrationInterface[]
+     */
+    protected function resolveIntegrationsFromUserConfig(ContainerInterface $container): array
+    {
+        $integrations = [
+            new Integration(),
+        ];
+        $userIntegrations = $container->get(ConfigInterface::class)->get('sentry.integrations', []);
+
+        foreach ($userIntegrations as $userIntegration) {
+            if ($userIntegration instanceof SdkIntegration\IntegrationInterface) {
+                $integrations[] = $userIntegration;
+            } elseif (\is_string($userIntegration)) {
+                $resolvedIntegration = make($userIntegration);
+
+                if (! $resolvedIntegration instanceof SdkIntegration\IntegrationInterface) {
+                    throw new RuntimeException('Sentry integrations should a instance of `\Sentry\Integration\IntegrationInterface`.');
+                }
+
+                $integrations[] = $resolvedIntegration;
+            } else {
+                throw new RuntimeException('Sentry integrations should either be a container reference or a instance of `\Sentry\Integration\IntegrationInterface`.');
+            }
+        }
+
+        return $integrations;
     }
 }
