8ef46ed120f70872e97abba222f6269e019cb21b
Deeka Wong <huangdijia@gmail.com>
2023-08-23 09:00:46 +0800

    Adds `TraceIntegration` (#315)
    Co-authored-by: Deeka Wong <8337659+huangdijia@users.noreply.github.com>

---
+++ src/sentry/src/Integration/TraceIntegration.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of friendsofhyperf/components.
+ *
+ * @link     https://github.com/friendsofhyperf/components
+ * @document https://github.com/friendsofhyperf/components/blob/3.0/README.md
+ * @contact  huangdijia@gmail.com
+ */
+
+namespace FriendsOfHyperf\Sentry\Integration;
+
+use Hyperf\Tracer\TracerContext;
+use Sentry\Event;
+use Sentry\Integration\IntegrationInterface;
+use Sentry\State\Scope;
+
+class TraceIntegration implements IntegrationInterface
+{
+    public function setupOnce(): void
+    {
+        Scope::addGlobalEventProcessor(function (Event $event): Event {
+            if (class_exists(TracerContext::class) && $span = TracerContext::getRoot()) {
+                /** @var \ZipkinOpenTracing\SpanContext $spanContext */
+                $spanContext = $span->getContext();
+                /** @var \Zipkin\Propagation\TraceContext $traceContext */
+                $traceContext = $spanContext->getContext();
+                $event->setContext('Hyperf Trace', [
+                    'Trace ID' => $traceContext->getTraceId(),
+                    'Parent ID' => $traceContext->getParentId(),
+                    'Span ID' => $traceContext->getSpanId(),
+                ]);
+            }
+
+            return $event;
+        });
+    }
+}
