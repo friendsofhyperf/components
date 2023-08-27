19e43df872f99757b39bf2184c4dbfa285cc61de
huangdijia <huangdijia@gmail.com>
2023-08-27 11:12:48 +0800

    Adds `di-plus` component

---
+++ src/di-plus/src/ConfigProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of friendsofhyperf/components.
+ *
+ * @link     https://github.com/friendsofhyperf/components
+ * @document https://github.com/friendsofhyperf/components/blob/3.0/README.md
+ * @contact  huangdijia@gmail.com
+ */
+
+namespace FriendsOfHyperf\DiPlus;
+
+use Hyperf\Di\Aop\RegisterInjectPropertyHandler;
+use Hyperf\Di\Definition\DefinitionSource;
+use Hyperf\Di\Definition\Reference;
+
+class ConfigProvider
+{
+    public function __invoke(): array
+    {
+        return [
+            'annotations' => [
+                'scan' => [
+                    'class_map' => [
+                        RegisterInjectPropertyHandler::class => __DIR__ . '/../class_map/RegisterInjectPropertyHandler.php',
+                        DefinitionSource::class => __DIR__ . '/../class_map/DefinitionSource.php',
+                        Reference::class => __DIR__ . '/../class_map/Reference.php',
+                    ],
+                ],
+            ],
+        ];
+    }
+}
