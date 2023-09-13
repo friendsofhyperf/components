b5a18a7d54b890a84f04340c7f13f32b3f0083bf
Deeka Wong <huangdijia@gmail.com>
2023-09-14 07:52:18 +0800

    Add `notModified` method to HTTP client (#336)
    Co-authored-by: Deeka Wong <8337659+huangdijia@users.noreply.github.com>

--- src/http-client/src/Concerns/DeterminesStatusCode.php
+++ src/http-client/src/Concerns/DeterminesStatusCode.php
@@ -75,6 +75,16 @@ trait DeterminesStatusCode
     }
 
     /**
+     * Determine if the response code was a 304 "Not Modified" response.
+     *
+     * @return bool
+     */
+    public function notModified()
+    {
+        return $this->status() === 304;
+    }
+
+    /**
      * Determine if the response was a 400 "Bad Request" response.
      *
      * @return bool
--- tests/HttpClient/HttpClientTest.php
+++ tests/HttpClient/HttpClientTest.php
@@ -67,6 +67,8 @@ class HttpClientTest extends TestCase
     protected function tearDown(): void
     {
         m::close();
+
+        parent::tearDown();
     }
 
     public function testStubbedResponsesAreReturnedAfterFaking()
@@ -146,6 +148,20 @@ class HttpClientTest extends TestCase
 
         $response = $this->factory->post('http://forge.laravel.com');
         $this->assertFalse($response->found());
+    }
+
+    public function testNotModifiedRequest(): void
+    {
+        $this->factory->fake([
+            'vapor.laravel.com' => $this->factory::response('', HttpResponse::HTTP_NOT_MODIFIED),
+            'forge.laravel.com' => $this->factory::response('', HttpResponse::HTTP_OK),
+        ]);
+
+        $response = $this->factory->post('https://vapor.laravel.com');
+        $this->assertTrue($response->notModified());
+
+        $response = $this->factory->post('https://forge.laravel.com');
+        $this->assertFalse($response->notModified());
     }
 
     public function testBadRequestRequest()
