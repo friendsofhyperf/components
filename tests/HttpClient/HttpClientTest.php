a2f5220c8ac41367fd01275d46a271dd91a414df
Deeka Wong <huangdijia@gmail.com>
2023-09-14 07:52:18 +0800

    Add `notModified` method to HTTP client (#336)
    Co-authored-by: Deeka Wong <8337659+huangdijia@users.noreply.github.com>

--- tests/HttpClient/HttpClientTest.php
+++ tests/HttpClient/HttpClientTest.php
@@ -66,6 +66,8 @@ class HttpClientTest extends TestCase
     protected function tearDown(): void
     {
         m::close();
+
+        parent::tearDown();
     }
 
     public function testStubbedResponsesAreReturnedAfterFaking()
@@ -145,6 +147,20 @@ class HttpClientTest extends TestCase
 
         $response = $this->factory->post('http://forge.laravel.com');
         $this->assertFalse($response->found());
+    }
+
+    public function testNotModifiedRequest(): void
+    {
+        $this->factory->fake([
+            'vapor.laravel.com' => $this->factory::response('', HttpResponse::HTTP_NOT_MODIFIED),
+            'forge.laravel.com' => $this->factory::response('', HttpResponse::HTTP_OK),
+        ]);
+
+        $response = $this->factory->post('https://vapor.laravel.com');
+        $this->assertTrue($response->notModified());
+
+        $response = $this->factory->post('https://forge.laravel.com');
+        $this->assertFalse($response->notModified());
     }
 
     public function testBadRequestRequest()
