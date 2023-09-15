cb231fb707ad3efbddb0311d2e1aea36abde61d8
Deeka Wong <huangdijia@gmail.com>
2023-09-15 20:08:43 +0800

    Use PHP native json_validate in isJson function if available (#338)

--- src/macros/src/StrMixin.php
+++ src/macros/src/StrMixin.php
@@ -140,6 +140,10 @@ class StrMixin
                 return false;
             }
 
+            if (function_exists('json_validate')) {
+                return json_validate($value);
+            }
+
             try {
                 json_decode($value, true, 512, JSON_THROW_ON_ERROR);
             } catch (JsonException $e) {
