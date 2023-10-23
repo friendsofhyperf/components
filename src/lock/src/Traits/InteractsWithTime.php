07516a5835cbbb78d19f9dbc535760ac3919bba0
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2023-10-23 23:03:42 +0800

    use `\Hyperf\Support\Traits\InteractsWithTime` instead of `FriendsOfHyperf\Lock\Traits\ InteractsWithTime`

--- src/lock/src/Traits/InteractsWithTime.php
+++ src/lock/src/Traits/InteractsWithTime.php
@@ -11,60 +11,10 @@ declare(strict_types=1);
 
 namespace FriendsOfHyperf\Lock\Traits;
 
-use Carbon\Carbon;
-use DateInterval;
-use DateTimeInterface;
-
+/**
+ * @deprecated since v3.0, will be removed in v3.1, please use \Hyperf\Support\Traits\InteractsWithTime instead.
+ */
 trait InteractsWithTime
 {
-    /**
-     * Get the number of seconds until the given DateTime.
-     *
-     * @param DateInterval|DateTimeInterface|int $delay
-     */
-    protected function secondsUntil($delay): int
-    {
-        $delay = $this->parseDateInterval($delay);
-
-        return $delay instanceof DateTimeInterface
-                            ? max(0, $delay->getTimestamp() - $this->currentTime())
-                            : (int) $delay;
-    }
-
-    /**
-     * Get the "available at" UNIX timestamp.
-     *
-     * @param DateInterval|DateTimeInterface|int $delay
-     */
-    protected function availableAt($delay = 0): int
-    {
-        $delay = $this->parseDateInterval($delay);
-
-        return $delay instanceof DateTimeInterface
-                            ? $delay->getTimestamp()
-                            : Carbon::now()->addSeconds($delay)->getTimestamp();
-    }
-
-    /**
-     * If the given value is an interval, convert it to a DateTime instance.
-     *
-     * @param DateInterval|DateTimeInterface|int $delay
-     * @return DateTimeInterface|int
-     */
-    protected function parseDateInterval($delay)
-    {
-        if ($delay instanceof DateInterval) {
-            $delay = Carbon::now()->add($delay);
-        }
-
-        return $delay;
-    }
-
-    /**
-     * Get the current system time as a UNIX timestamp.
-     */
-    protected function currentTime(): int
-    {
-        return Carbon::now()->getTimestamp();
-    }
+    use \Hyperf\Support\Traits\InteractsWithTime;
 }
