e00a81198bc89e3f8747356a2a7c1695c67e3ab5
greezen <greezen@addcn.com>
2023-11-08 10:35:01 +0000

    perf: Timer add log

--- src/confd/src/Confd.php
+++ src/confd/src/Confd.php
@@ -17,9 +17,11 @@ use FriendsOfHyperf\Confd\Event\ConfigChanged;
 use FriendsOfHyperf\Confd\Event\WatchDispatched;
 use Hyperf\Collection\Arr;
 use Hyperf\Contract\ConfigInterface;
+use Hyperf\Contract\StdoutLoggerInterface;
 use Hyperf\Coordinator\Timer;
 use Psr\Container\ContainerInterface;
 use Psr\EventDispatcher\EventDispatcherInterface;
+use Swoole\FastCGI\Record\Stdout;
 
 class Confd
 {
@@ -31,13 +33,17 @@ class Confd
 
     private array $previous = [];
 
-    public function __construct(private ContainerInterface $container, private ConfigInterface $config)
+    public function __construct(
+        private ContainerInterface $container, 
+        private ConfigInterface $config,
+        private StdoutLoggerInterface $logger
+        )
     {
         $driver = $this->config->get('confd.default', 'etcd');
         $class = $this->config->get(sprintf('confd.drivers.%s.driver', $driver), Etcd::class);
         $this->driver = $container->get($class);
         $this->interval = (int) $this->config->get('confd.interval', 1);
-        $this->timer = new Timer();
+        $this->timer = new Timer($logger);
     }
 
     public function fetch(): array
