fc7eee042e58b4cf79a00ec82dbd7dcc7cfbbae7
Siam <59419979@qq.com>
2023-11-17 10:01:56 +0000

    Optimize sentry config.

--- src/sentry/publish/sentry.php
+++ src/sentry/publish/sentry.php
@@ -25,6 +25,9 @@ return [
     // @see: https://docs.sentry.io/platforms/php/guides/laravel/configuration/options/#sample-rate
     'sample_rate' => env('SENTRY_SAMPLE_RATE') === null ? 1.0 : (float) env('SENTRY_SAMPLE_RATE'),
 
+    // Switch tracing on/off
+    'enable_tracing' => env('SENTRY_ENABLE_TRACING', true),
+
     // @see: https://docs.sentry.io/platforms/php/guides/laravel/configuration/options/#traces-sample-rate
     'traces_sample_rate' => env('SENTRY_TRACES_SAMPLE_RATE') === null ? 1.0 : (float) env('SENTRY_TRACES_SAMPLE_RATE'),
 
@@ -38,9 +41,6 @@ return [
 
     // @see: https://docs.sentry.io/platforms/php/guides/laravel/configuration/options/#profiles-sample-rate
     'profiles_sample_rate' => env('SENTRY_PROFILES_SAMPLE_RATE') === null ? null : (float) env('SENTRY_PROFILES_SAMPLE_RATE'),
-
-    // Switch tracing on/off
-    'enable_tracing' => env('SENTRY_ENABLE_TRACING', true),
 
     // @see: https://docs.sentry.io/platforms/php/guides/laravel/configuration/options/#send-default-pii
     'send_default_pii' => env('SENTRY_SEND_DEFAULT_PII', false),
