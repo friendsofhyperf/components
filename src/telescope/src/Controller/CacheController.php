f5cc5cc05a4965fb2f203177a478453b00322c90
guandeng <guandeng@Hotmail.com>
2023-11-18 15:36:08 +0800

    Add record cache data (#438)
    * Add record cache data
    
    * Reserve CACHE_PACKER
    
    * Add deprecated for telescope cache_packer context
    
    * optimized code

---
+++ src/telescope/src/Controller/CacheController.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of friendsofhyperf/components.
+ *
+ * @link     https://github.com/friendsofhyperf/components
+ * @document https://github.com/friendsofhyperf/components/blob/main/README.md
+ * @contact  huangdijia@gmail.com
+ */
+
+namespace FriendsOfHyperf\Telescope\Controller;
+
+use FriendsOfHyperf\Telescope\EntryType;
+use Hyperf\HttpServer\Annotation\Controller;
+use Hyperf\HttpServer\Annotation\GetMapping;
+use Hyperf\HttpServer\Annotation\PostMapping;
+
+#[Controller(server: 'telescope')]
+class CacheController extends EntryController
+{
+    #[PostMapping(path: '/telescope/telescope-api/cache')]
+    public function list()
+    {
+        return $this->index();
+    }
+
+    #[GetMapping(path: '/telescope/telescope-api/cache/{id}')]
+    public function detail(string $id)
+    {
+        return $this->show($id);
+    }
+
+    /**
+     * The entry type for the controller.
+     *
+     * @return string
+     */
+    protected function entryType()
+    {
+        return EntryType::CACHE;
+    }
+
+    /**
+     * The watcher class for the controller.
+     *
+     * @return string
+     */
+    protected function watcher()
+    {
+        // return RequestWatcher::class;
+        return null;
+    }
+}
