f0ea0e071ed99f3fee3250d2ec4417467db69536
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2023-11-20 11:00:25 +0800

    Adds CacheAspect
    Co-Authored-By: Deeka Wong <8337659+huangdijia@users.noreply.github.com>

--- src/sentry/src/ConfigProvider.php
+++ src/sentry/src/ConfigProvider.php
@@ -20,6 +20,7 @@ class ConfigProvider
         return [
             'aspects' => [
                 Aspect\BreadcrumbAspect::class,
+                Aspect\CacheAspect::class,
                 Aspect\CoroutineAspect::class,
                 Aspect\CrontabExecutorAspect::class,
                 Aspect\GuzzleHttpClientAspect::class,
