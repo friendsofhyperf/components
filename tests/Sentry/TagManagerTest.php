957751a78f317651ae05f22bdb0da4fdf8e1ed14
Deeka Wong <huangdijia@gmail.com>
2023-12-17 15:33:11 +0800

    Optimize TagManager (#493)
    * Optimize TagManager
    
    * Refactor buildTagKey method to use sprintf for string concatenation
    
    * Add TagManagerTest.php unit test
    
    ---------
    
    Co-authored-by: Deeka Wong <8337659+huangdijia@users.noreply.github.com>

---
+++ tests/Sentry/TagManagerTest.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of friendsofhyperf/components.
+ *
+ * @link     https://github.com/friendsofhyperf/components
+ * @document https://github.com/friendsofhyperf/components/blob/main/README.md
+ * @contact  huangdijia@gmail.com
+ */
+
+namespace FriendsOfHyperf\Tests\Sentry;
+
+use FriendsOfHyperf\Sentry\Tracing\TagManager;
+use Hyperf\Config\Config;
+
+beforeEach(function () {
+    $config = new Config([
+        'sentry' => [
+            'tracing' => [
+                'tags' => [
+                    'foo' => [
+                        'bar' => 'foo.bar',
+                        'baz' => 'foo.baz',
+                    ],
+                ],
+            ],
+        ],
+    ]);
+    $this->tagManager = new TagManager($config);
+});
+
+test('test has', function ($key, $expected) {
+    expect($this->tagManager->has($key))->toBe($expected);
+})->with([
+    ['foo.bar', true],
+    ['foo.baz', true],
+    ['foo.bay', false],
+]);
+
+test('test get', function ($key, $expected) {
+    expect($this->tagManager->get($key))->toBe($expected);
+})->with([
+    ['foo.bar', 'foo.bar'],
+    ['foo.baz', 'foo.baz'],
+    ['foo.bay', 'foo.bay'],
+]);
