e76da602e2c81216495b7e317236193a097736fe
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2023-12-20 10:08:46 +0800

    Update type hint for process method in TaskHandleListener

--- src/async-task/src/Listener/TaskHandleListener.php
+++ src/async-task/src/Listener/TaskHandleListener.php
@@ -42,7 +42,7 @@ class TaskHandleListener implements ListenerInterface
     }
 
     /**
-     * @param FailedHandle $event
+     * @param FailedHandle|object $event
      */
     public function process(object $event): void
     {
