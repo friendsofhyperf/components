941f7c5fb8703c2cb2c0e5c011268751029f6d0a
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2023-12-27 00:09:59 +0800

    Refactor GrpcCoreMiddlewareAspect to handle request and response payloads

--- src/telescope/src/Aspect/GrpcCoreMiddlewareAspect.php
+++ src/telescope/src/Aspect/GrpcCoreMiddlewareAspect.php
@@ -34,46 +34,52 @@ class GrpcCoreMiddlewareAspect extends AbstractAspect
 
     public function process(ProceedingJoinPoint $proceedingJoinPoint)
     {
-        if (! $this->telescopeConfig->isEnable('grpc')) {
-            return $proceedingJoinPoint->process();
-        }
-        return match ($proceedingJoinPoint->methodName) {
-            'parseMethodParameters' => $this->processParseMethodParameters($proceedingJoinPoint),
-            'handleResponse' => $this->processHandleResponse($proceedingJoinPoint),
-            default => $proceedingJoinPoint->process(),
-        };
-    }
-
-    // 处理请求
-    protected function processParseMethodParameters(ProceedingJoinPoint $proceedingJoinPoint)
-    {
-        return tap($proceedingJoinPoint->process(), function ($result) {
-            try {
-                /** @var Message $message */
-                $message = $result[0];
-                $payload = json_decode($message->serializeToJsonString(), true);
-            } catch (Throwable $e) {
-                return;
-            }
-            TelescopeContext::setGrpcRequestPayload($payload);
-        });
-    }
-
-    // 处理响应
-    protected function processHandleResponse(ProceedingJoinPoint $proceedingJoinPoint)
-    {
         return tap($proceedingJoinPoint->process(), function ($result) use ($proceedingJoinPoint) {
-            /** @var Message|null $message */
-            $message = $proceedingJoinPoint->arguments['keys']['message'] ?? null;
-            if (is_null($message)) {
+            if (! $this->telescopeConfig->isEnable('grpc')) {
                 return;
             }
-            try {
-                $payload = json_decode($message->serializeToJsonString(), true);
-            } catch (Throwable $e) {
-                return;
-            }
-            TelescopeContext::setGrpcResponsePayload($payload);
+
+            match ($proceedingJoinPoint->methodName) {
+                'parseMethodParameters' => $this->setRequestPayload($result[0] ?? null),
+                'handleResponse' => $this->setResponsePayload($proceedingJoinPoint->arguments['keys']['message'] ?? null),
+                default => null,
+            };
         });
+    }
+
+    /**
+     * @param Message|null $message
+     */
+    protected function setRequestPayload($message)
+    {
+        if (! $message instanceof Message) {
+            return;
+        }
+
+        try {
+            $payload = json_decode($message->serializeToJsonString(), true);
+        } catch (Throwable $e) {
+            return;
+        }
+
+        TelescopeContext::setGrpcRequestPayload($payload);
+    }
+
+    /**
+     * @param Message|null $message
+     */
+    protected function setResponsePayload($message)
+    {
+        if (! $message instanceof Message) {
+            return;
+        }
+
+        try {
+            $payload = json_decode($message->serializeToJsonString(), true);
+        } catch (Throwable $e) {
+            return;
+        }
+
+        TelescopeContext::setGrpcResponsePayload($payload);
     }
 }
