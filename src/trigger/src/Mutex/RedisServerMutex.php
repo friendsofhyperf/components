9beab8e01361ad3c48d47724c0bd956a0e12328f
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2024-02-03 22:20:32 +0800

    Fix RedisServerMutex connection assignment

--- src/trigger/src/Mutex/RedisServerMutex.php
+++ src/trigger/src/Mutex/RedisServerMutex.php
@@ -48,7 +48,9 @@ class RedisServerMutex implements ServerMutexInterface
         $this->keepaliveInterval = (int) ($options['keepalive_interval'] ?? 10);
         $this->name = $name ?? sprintf('trigger:server:%s', $this->connection);
         $this->owner = $owner ?? Util::getInternalIp();
-        $this->connection = $options['connection'];
+        if (isset($options['connection'])) {
+            $this->connection = $options['connection'];
+        }
         $this->logger = $this->getLogger();
         $this->timer = new Timer($this->logger);
         $this->retryInterval = (int) ($options['retry_interval'] ?? 10);
