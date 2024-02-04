7d14bfdf382fa397abf93fe889d791443eee1450
Deeka Wong <huangdijia@gmail.com>
2024-02-04 09:24:36 +0800

    Refactor TriggerSubscriber class to improve performance and add channel handling (#546)

--- src/trigger/src/Subscriber/TriggerSubscriber.php
+++ src/trigger/src/Subscriber/TriggerSubscriber.php
@@ -11,10 +11,15 @@ declare(strict_types=1);
 
 namespace FriendsOfHyperf\Trigger\Subscriber;
 
+use Closure;
 use FriendsOfHyperf\Trigger\Consumer;
 use FriendsOfHyperf\Trigger\Traits\Logger;
 use FriendsOfHyperf\Trigger\TriggerManager;
+use Hyperf\Coordinator\Constants;
+use Hyperf\Coordinator\CoordinatorManager;
 use Hyperf\Coroutine\Concurrent;
+use Hyperf\Engine\Channel;
+use Hyperf\Engine\Coroutine;
 use MySQLReplication\Definitions\ConstEventsNames;
 use MySQLReplication\Event\DTO\EventDTO;
 use MySQLReplication\Event\DTO\RowsDTO;
@@ -32,6 +37,10 @@ class TriggerSubscriber extends AbstractSubscriber
 
     protected ?LoggerInterface $logger = null;
 
+    protected ?Channel $chan = null;
+
+    protected int $channelSize = 65535;
+
     public function __construct(
         protected ContainerInterface $container,
         protected TriggerManager $triggerManager,
@@ -40,6 +49,9 @@ class TriggerSubscriber extends AbstractSubscriber
         $this->concurrent = new Concurrent(
             (int) ($consumer->getOption('concurrent.limit') ?? 1)
         );
+        if ($consumer->getOption('channel.size')) {
+            $this->channelSize = (int) $consumer->getOption('channel.size');
+        }
         $this->logger = $this->getLogger();
     }
 
@@ -52,11 +64,62 @@ class TriggerSubscriber extends AbstractSubscriber
         ];
     }
 
+    protected function close(): void
+    {
+        $this->chan?->close();
+        $this->chan = null;
+    }
+
+    protected function loop(): void
+    {
+        if ($this->chan) {
+            return;
+        }
+
+        $this->chan = new Channel($this->channelSize);
+
+        Coroutine::create(function () {
+            try {
+                while (true) {
+                    while (true) {
+                        /** @var Closure|null $closure */
+                        $closure = $this->chan?->pop();
+
+                        if ($closure === null) {
+                            break 2;
+                        }
+
+                        try {
+                            $this->concurrent->create($closure);
+                        } catch (Throwable $e) {
+                            $this->error((string) $e);
+                            break;
+                        } finally {
+                            $closure = null;
+                        }
+                    }
+                }
+            } catch (Throwable $e) {
+                $this->error((string) $e);
+            } finally {
+                $this->close();
+            }
+        });
+
+        Coroutine::create(function () {
+            if (CoordinatorManager::until(Constants::WORKER_EXIT)->yield()) {
+                $this->close();
+            }
+        });
+    }
+
     protected function allEvents(EventDTO $event): void
     {
         if (! $event instanceof RowsDTO) {
             return;
         }
+
+        $this->loop();
 
         $key = join('.', [
             $this->consumer->getConnection(),
@@ -69,7 +132,7 @@ class TriggerSubscriber extends AbstractSubscriber
 
         foreach ($this->triggerManager->get($key) as $callable) {
             foreach ($event->getValues() as $value) {
-                $this->concurrent->create(function () use ($callable, $value, $eventType) {
+                $this->chan->push(function () use ($callable, $value, $eventType) {
                     [$class, $method] = $callable;
 
                     if (! $this->container->has($class)) {
