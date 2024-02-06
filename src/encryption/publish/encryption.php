7e35fddb90ad0f02bda0ad88f09d4ae31f23d074
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2024-02-06 11:16:06 +0800

    Update encryption configuration

--- src/encryption/publish/encryption.php
+++ src/encryption/publish/encryption.php
@@ -8,7 +8,9 @@ declare(strict_types=1);
  * @document https://github.com/friendsofhyperf/components/blob/main/README.md
  * @contact  huangdijia@gmail.com
  */
+use function Hyperf\Support\env;
+
 return [
-    'key' => 'base64:MhEHk72OcV2ttAljUu9Caaam3iP2BnGcwb6GWKkUfV4=',
-    'cipher' => 'AES-256-CBC',
+    'key' => (string) env('APP_KEY', 'base64:MhEHk72OcV2ttAljUu9Caaam3iP2BnGcwb6GWKkUfV4='),
+    'cipher' => (string) env('APP_CIPHER', 'AES-256-CBC'),
 ];
