0ba68ece21404a7996e2a34930b71f2c642f450e
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2024-02-22 17:46:47 +0800

    Add optional scene parameter to data resolver methods

--- src/validated-dto/src/Concerns/DataResolver.php
+++ src/validated-dto/src/Concerns/DataResolver.php
@@ -24,22 +24,22 @@ trait DataResolver
     /**
      * @throws InvalidJsonException|ValidationException|MissingCastTypeException|CastTargetException
      */
-    public static function fromJson(string $json): static
+    public static function fromJson(string $json, ?string $scene = null): static
     {
         $jsonDecoded = json_decode($json, true);
         if (! is_array($jsonDecoded)) {
             throw new InvalidJsonException();
         }
 
-        return new static($jsonDecoded);
+        return new static($jsonDecoded, $scene);
     }
 
     /**
      * @throws CastTargetException|MissingCastTypeException
      */
-    public static function fromArray(array $array): static
+    public static function fromArray(array $array, ?string $scene = null): static
     {
-        return new static($array);
+        return new static($array, $scene);
     }
 
     /**
@@ -47,45 +47,45 @@ trait DataResolver
      * @param \Hyperf\HttpServer\Contract\RequestInterface $request
      * @throws ValidationException|MissingCastTypeException|CastTargetException
      */
-    public static function fromRequest(RequestInterface $request): static
+    public static function fromRequest(RequestInterface $request, ?string $scene = null): static
     {
-        return new static($request->all());
+        return new static($request->all(), $scene);
     }
 
     /**
      * @throws ValidationException|MissingCastTypeException|CastTargetException
      */
-    public static function fromModel(Model $model): static
+    public static function fromModel(Model $model, ?string $scene = null): static
     {
-        return new static($model->toArray());
+        return new static($model->toArray(), $scene);
     }
 
     /**
      * @throws ValidationException|MissingCastTypeException|CastTargetException
      */
-    public static function fromCommandArguments(Command $command): static
+    public static function fromCommandArguments(Command $command, ?string $scene = null): static
     {
         $arguments = (fn () => $this->input->getArguments())->call($command);
-        return new static(self::filterArguments($arguments));
+        return new static(self::filterArguments($arguments), $scene);
     }
 
     /**
      * @throws ValidationException|MissingCastTypeException|CastTargetException
      */
-    public static function fromCommandOptions(Command $command): static
+    public static function fromCommandOptions(Command $command, ?string $scene = null): static
     {
         $options = (fn () => $this->input->getOptions())->call($command);
-        return new static($options);
+        return new static($options, $scene);
     }
 
     /**
      * @throws ValidationException|MissingCastTypeException|CastTargetException
      */
-    public static function fromCommand(Command $command): static
+    public static function fromCommand(Command $command, ?string $scene = null): static
     {
         $arguments = (fn () => $this->input->getArguments())->call($command);
         $options = (fn () => $this->input->getOptions())->call($command);
-        return new static(array_merge(self::filterArguments($arguments), $options));
+        return new static(array_merge(self::filterArguments($arguments), $options), $scene);
     }
 
     private static function filterArguments(array $arguments): array
