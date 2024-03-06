3156fc05771a94d71e1f0100b7c07e7b9c53325d
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2024-03-06 14:08:34 +0800

    Update PHPStorm meta file and remove unused code

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,19 +5,13 @@ declare(strict_types=1);
  * This file is part of friendsofhyperf/components.
  *
  * @link     https://github.com/friendsofhyperf/components
- * @document https://github.com/friendsofhyperf/components/blob/3.x/README.md
+ * @document https://github.com/friendsofhyperf/components/blob/main/README.md
  * @contact  huangdijia@gmail.com
  */
+
 namespace PHPSTORM_META;
 
 // Reflect
-override(\app(0), map(['' => '@']));
-override(\di(0), map(['' => '@']));
-override(\resolve(0), map(['' => '@']));
-override(\make(0), map(['' => '@']));
-override(\optional(0), type(0));
-override(\tap(0), type(0));
-
 override(\FriendsOfHyperf\Helpers\app(0), map(['' => '@']));
 override(\FriendsOfHyperf\Helpers\di(0), map(['' => '@']));
 override(\Hyperf\Context\Context::get(0), map(['' => '@']));
@@ -25,3 +19,8 @@ override(\Hyperf\Support\make(0), map(['' => '@']));
 override(\Hyperf\Support\optional(0), type(0));
 override(\Hyperf\Tappable\tap(0), type(0));
 override(\Psr\Container\ContainerInterface::get(0), map(['' => '@']));
+override(\FriendsOfHyperf\Tests\Concerns\InteractsWithContainer::mock(0), map(['' => '@']));
+override(\FriendsOfHyperf\Tests\Concerns\InteractsWithContainer::swap(0), map(['' => '@']));
+override(\FriendsOfHyperf\Tests\Concerns\InteractsWithContainer::instance(0), map(['' => '@']));
+override(\FriendsOfHyperf\Tests\Concerns\InteractsWithContainer::partialMock(0), map(['' => '@']));
+override(\FriendsOfHyperf\Tests\Concerns\InteractsWithContainer::spy(0), map(['' => '@']));
