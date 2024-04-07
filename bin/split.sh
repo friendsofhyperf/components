8f31fc50b047e25648b30d759a0657c1149e2f10
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2024-04-07 11:29:10 +0800

    Refactor split.sh to split the .github directory into a separate repository

--- bin/split.sh
+++ bin/split.sh
@@ -30,6 +30,8 @@ if [[ $# -eq 0 ]]; then
     REPOS=$(ls $BASEPATH)
 fi
 
+split "src/.github" "git@github.com:friendsofhyperf/.github.git"
+
 for REPO in $REPOS ; do
     remote $REPO git@github.com:friendsofhyperf/${REPO}.git
 
