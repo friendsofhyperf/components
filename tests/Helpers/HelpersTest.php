12765c3b74f5173e3e5ba486ce27ee3563983821
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2024-05-08 09:23:27 +0800

    feat: Add tests for blank and filled functions with stringable support

--- tests/Helpers/HelpersTest.php
+++ tests/Helpers/HelpersTest.php
@@ -65,30 +65,34 @@ test('test FriendsOfHyperf\Helpers\Command\call', function () {
     expect(call('foo:bar', ['argument' => 'value']))->toBe(0);
 });
 
-test('filled', function () {
-    $this->assertFalse(filled(null));
-    $this->assertFalse(filled(''));
-    $this->assertFalse(filled('  '));
-    $this->assertFalse(filled(new Stringable('')));
-    $this->assertFalse(filled(new Stringable('  ')));
-    $this->assertTrue(filled(10));
-    $this->assertTrue(filled(true));
-    $this->assertTrue(filled(false));
-    $this->assertTrue(filled(0));
-    $this->assertTrue(filled(0.0));
-    $this->assertTrue(filled(new Stringable(' FooBar ')));
-});
+test('test filled', function ($expect, $value) {
+    expect(filled($value))->toBe($expect);
+})->with([
+    [false, null],
+    [false, ''],
+    [false, '  '],
+    [false, new Stringable('')],
+    [false, new Stringable('  ')],
+    [true, 10],
+    [true, true],
+    [true, false],
+    [true, 0],
+    [true, 0.0],
+    [true, new Stringable(' FooBar ')],
+]);
 
-test('blank', function () {
-    $this->assertTrue(blank(null));
-    $this->assertTrue(blank(''));
-    $this->assertTrue(blank('  '));
-    $this->assertTrue(blank(new Stringable('')));
-    $this->assertTrue(blank(new Stringable('  ')));
-    $this->assertFalse(blank(10));
-    $this->assertFalse(blank(true));
-    $this->assertFalse(blank(false));
-    $this->assertFalse(blank(0));
-    $this->assertFalse(blank(0.0));
-    $this->assertFalse(blank(new Stringable(' FooBar ')));
-});
+test('test blank', function ($expect, $value) {
+    expect(blank($value))->toBe($expect);
+})->with([
+    [true, null],
+    [true, ''],
+    [true, '  '],
+    [true, new Stringable('')],
+    [true, new Stringable('  ')],
+    [false, 10],
+    [false, true],
+    [false, false],
+    [false, 0],
+    [false, 0.0],
+    [false, new Stringable(' FooBar ')],
+]);
