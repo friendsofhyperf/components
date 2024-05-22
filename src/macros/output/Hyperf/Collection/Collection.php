a745b055172daf34a5790b17c70f4904098ab673
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2024-05-22 09:29:56 +0800

    feat: Remove unnecessary methods from Collection

--- src/macros/output/Hyperf/Collection/Collection.php
+++ src/macros/output/Hyperf/Collection/Collection.php
@@ -11,38 +11,8 @@ declare(strict_types=1);
 
 namespace Hyperf\Collection;
 
-use Hyperf\Contract\Arrayable;
-use UnexpectedValueException;
-
 class Collection
 {
-    /**
-     * Ensure that every item in the collection is of the expected type.
-     *
-     * @template TEnsureOfType
-     *
-     * @param class-string<TEnsureOfType> $type
-     * @return static<mixed, TEnsureOfType>
-     *
-     * @throws UnexpectedValueException
-     */
-    public function ensure($type)
-    {
-    }
-
-    /**
-     * Get the first item in the collection but throw an exception if no matching items exist.
-     *
-     * @param string $key
-     * @param mixed $operator
-     * @param mixed $value
-     * @return mixed
-     * @throws ItemNotFoundException
-     */
-    public function firstOrFail($key = null, $operator = null, $value = null)
-    {
-    }
-
     /**
      * Get an item from the collection by key or add it to collection if it does not exist.
      *
@@ -64,120 +34,11 @@ class Collection
     }
 
     /**
-     * Intersect the collection with the given items, using the callback.
-     *
-     * @param Arrayable<array-key, TValue>|iterable<array-key, TValue> $items
-     * @param callable(TValue, TValue): int $callback
-     * @return static
-     */
-    public function intersectUsing($items, callable $callback)
-    {
-    }
-
-    /**
-     * Intersect the collection with the given items with additional index check, using the callback.
-     *
-     * @param Arrayable<array-key, TValue>|iterable<array-key, TValue> $items
-     * @param callable(TValue, TValue): int $callback
-     * @return static
-     */
-    public function intersectAssocUsing($items, callable $callback)
-    {
-    }
-
-    /**
-     * Pass the collection through a series of callable pipes and return the result.
-     *
-     * @param array<callable> $pipes
-     * @return mixed
-     */
-    public function pipeThrough($pipes)
-    {
-    }
-
-    /**
-     * Get the first item in the collection, but only if exactly one item exists. Otherwise, throw an exception.
-     *
-     * @param string $key
-     * @param mixed $operator
-     * @param mixed $value
-     * @return mixed
-     * @throws ItemNotFoundException
-     * @throws MultipleItemsFoundException
-     */
-    public function sole($key = null, $operator = null, $value = null)
-    {
-    }
-
-    /**
      * Convert a flatten "dot" notation array into an expanded array.
      *
      * @return Collection
      */
     public function undot()
-    {
-    }
-
-    /**
-     * Get a single key's value from the first matching item in the collection.
-     *
-     * @param string $key
-     * @param mixed $default
-     * @return mixed
-     */
-    public function value($key, $default = null)
-    {
-    }
-
-    /**
-     * Apply the callback if the collection is empty.
-     *
-     * @template TWhenEmptyReturnType
-     *
-     * @param (callable($this): TWhenEmptyReturnType) $callback
-     * @param (callable($this): TWhenEmptyReturnType)|null $default
-     * @return $this|TWhenEmptyReturnType
-     */
-    public function whenEmpty(callable $callback, ?callable $default = null)
-    {
-    }
-
-    /**
-     * Apply the callback if the collection is not empty.
-     *
-     * @template TWhenNotEmptyReturnType
-     *
-     * @param callable($this): TWhenNotEmptyReturnType $callback
-     * @param (callable($this): TWhenNotEmptyReturnType)|null $default
-     * @return $this|TWhenNotEmptyReturnType
-     */
-    public function whenNotEmpty(callable $callback, ?callable $default = null)
-    {
-    }
-
-    /**
-     * Apply the callback unless the collection is empty.
-     *
-     * @template TUnlessEmptyReturnType
-     *
-     * @param callable($this): TUnlessEmptyReturnType $callback
-     * @param (callable($this): TUnlessEmptyReturnType)|null $default
-     * @return $this|TUnlessEmptyReturnType
-     */
-    public function unlessEmpty(callable $callback, ?callable $default = null)
-    {
-    }
-
-    /**
-     * Apply the callback unless the collection is not empty.
-     *
-     * @template TUnlessNotEmptyReturnType
-     *
-     * @param callable($this): TUnlessNotEmptyReturnType $callback
-     * @param (callable($this): TUnlessNotEmptyReturnType)|null $default
-     * @return $this|TUnlessNotEmptyReturnType
-     */
-    public function unlessNotEmpty(callable $callback, ?callable $default = null)
     {
     }
 }
