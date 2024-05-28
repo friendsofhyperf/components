4ee54ba8d16d01ef67fe042ebff9006a39b33128
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2024-05-28 16:06:31 +0800

    Optimized LongCast and DoubleCast

--- tests/ValidatedDTO/Unit/LongCastTest.php
+++ tests/ValidatedDTO/Unit/LongCastTest.php
@@ -16,7 +16,17 @@ it('properly casts to integer')
     ->cast(test_property(), '5')
     ->toBe(5);
 
-it('throws exception when it is unable to cast property')
+it('throws exception when it is string')
     ->expect(fn () => new LongCast())
     ->cast(test_property(), 'TEST')
+    ->toBe(0);
+
+it('throws exception when it is empty string')
+    ->expect(fn () => new LongCast())
+    ->cast(test_property(), '')
+    ->toBe(0);
+
+it('throws exception when it is unable to cast property')
+    ->expect(fn () => new LongCast())
+    ->cast(test_property(), new stdClass())
     ->throws(CastException::class, 'Unable to cast property: test_property - invalid value');
