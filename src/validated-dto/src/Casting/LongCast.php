d893bf14efae43f2c4fea698537f9f54e4e36de0
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2024-05-28 15:41:47 +0800

    Adds LongCast and DoubleCast

---
+++ src/validated-dto/src/Casting/LongCast.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of friendsofhyperf/components.
+ *
+ * @link     https://github.com/friendsofhyperf/components
+ * @document https://github.com/friendsofhyperf/components/blob/main/README.md
+ * @contact  huangdijia@gmail.com
+ */
+
+namespace FriendsOfHyperf\ValidatedDTO\Casting;
+
+use FriendsOfHyperf\ValidatedDTO\Exception\CastException;
+
+class LongCast implements Castable
+{
+    /**
+     * @throws CastException
+     */
+    public function cast(string $property, mixed $value): int
+    {
+        return (int) $value;
+    }
+}
