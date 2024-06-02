c66c9d274afa54eb7cf1c7cb4be6fbe2d500b19c
Zds <49744633+zds-s@users.noreply.github.com>
2024-06-02 16:12:21 +0800

    Register Symfony channel

---
+++ src/notification-symfony/src/Listener/RegisterChannelListener.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of friendsofhyperf/components.
+ *
+ * @link     https://github.com/friendsofhyperf/components
+ * @document https://github.com/friendsofhyperf/components/blob/main/README.md
+ * @contact  huangdijia@gmail.com
+ */
+
+namespace FriendsOfHyperf\Notification\Symfony\Listener;
+
+use FriendsOfHyperf\Notification\ChannelManager;
+use FriendsOfHyperf\Notification\Symfony\Channel\SymfonyChannel;
+use Hyperf\Event\Contract\ListenerInterface;
+use Hyperf\Framework\Event\BootApplication;
+
+class RegisterChannelListener implements ListenerInterface
+{
+    public function __construct(
+        private readonly ChannelManager $channelManager
+    ) {
+    }
+
+    public function listen(): array
+    {
+        return [
+            BootApplication::class,
+        ];
+    }
+
+    public function process(object $event): void
+    {
+        $this->channelManager->register('symfony', SymfonyChannel::class);
+    }
+}
