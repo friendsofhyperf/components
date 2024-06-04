f6e0ade5d01140272c13cbea8dc680342d4ceb7d
Zds <49744633+zds-s@users.noreply.github.com>
2024-06-04 17:17:11 +0800

    Added AttachableTest

---
+++ tests/Mail/AttachableTest.php
@@ -0,0 +1,120 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of friendsofhyperf/components.
+ *
+ * @link     https://github.com/friendsofhyperf/components
+ * @document https://github.com/friendsofhyperf/components/blob/main/README.md
+ * @contact  huangdijia@gmail.com
+ */
+
+namespace FriendsOfHyperf\Tests\Mail;
+
+use FriendsOfHyperf\Mail\Attachment;
+use FriendsOfHyperf\Mail\Contract\Attachable;
+use FriendsOfHyperf\Mail\Mailable;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @internal
+ * @coversNothing
+ */
+class AttachableTest extends TestCase
+{
+    public function testItCanHaveMacroConstructors(): void
+    {
+        Attachment::macro('fromInvoice', function ($name) {
+            return Attachment::fromData(fn () => 'pdf content', $name);
+        });
+        $mailable = new Mailable();
+
+        $mailable->attach(new class() implements Attachable {
+            public function toMailAttachment(): Attachment
+            {
+                return Attachment::fromInvoice('foo')
+                    ->as('bar')
+                    ->withMime('image/jpeg');
+            }
+        });
+
+        $this->assertSame([
+            'data' => 'pdf content',
+            'name' => 'bar',
+            'options' => [
+                'mime' => 'image/jpeg',
+            ],
+        ], $mailable->rawAttachments[0]);
+    }
+
+    public function testItCanUtiliseExistingApisOnNonMailBasedResourcesWithPath(): void
+    {
+        Attachment::macro('size', function () {
+            return 99;
+        });
+        $notification = new class() {
+            public $pathArgs;
+
+            public function withPathAttachment()
+            {
+                $this->pathArgs = func_get_args();
+            }
+        };
+        $attachable = new class() implements Attachable {
+            public function toMailAttachment(): Attachment
+            {
+                return Attachment::fromPath('foo.jpg')
+                    ->as('bar')
+                    ->withMime('text/css');
+            }
+        };
+
+        $attachable->toMailAttachment()->attachWith(
+            fn ($path, $attachment) => $notification->withPathAttachment($path, $attachment->as, $attachment->mime, $attachment->size()),
+            fn () => null
+        );
+
+        $this->assertSame([
+            'foo.jpg',
+            'bar',
+            'text/css',
+            99,
+        ], $notification->pathArgs);
+    }
+
+    public function testItCanUtiliseExistingApisOnNonMailBasedResourcesWithArgs(): void
+    {
+        Attachment::macro('size', function () {
+            return 99;
+        });
+        $notification = new class() {
+            public $pathArgs;
+
+            public $dataArgs;
+
+            public function withDataAttachment(): void
+            {
+                $this->dataArgs = func_get_args();
+            }
+        };
+        $attachable = new class() implements Attachable {
+            public function toMailAttachment(): Attachment
+            {
+                return Attachment::fromData(fn () => 'expected attachment body', 'bar')
+                    ->withMime('text/css');
+            }
+        };
+
+        $attachable->toMailAttachment()->attachWith(
+            fn () => null,
+            fn ($data, $attachment) => $notification->withDataAttachment($data(), $attachment->as, $attachment->mime, $attachment->size()),
+        );
+
+        $this->assertSame([
+            'expected attachment body',
+            'bar',
+            'text/css',
+            99,
+        ], $notification->dataArgs);
+    }
+}
