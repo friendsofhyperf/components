d35fdad118a30554be8af814e6aa8a0c460baa3f
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2024-06-04 10:38:47 +0800

    chore: Update mail package to use MessageSendingStatus instead of MessageSendingStatusEnum

--- src/mail/src/Enums/MessageSendingStatus.php
+++ src/mail/src/Enums/MessageSendingStatus.php
@@ -11,7 +11,7 @@ declare(strict_types=1);
 
 namespace FriendsOfHyperf\Mail\Enums;
 
-enum MessageSendingStatusEnum: int
+enum MessageSendingStatus: int
 {
     /**
      * The message was sent successfully.
