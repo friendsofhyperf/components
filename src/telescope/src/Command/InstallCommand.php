6a97bd25631d026b990061bda5b482c432bf87e7
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2024-06-04 11:25:53 +0800

    chore: Update InstallCommand to use `call` method for vendor:publish and migrate commands

--- src/telescope/src/Command/InstallCommand.php
+++ src/telescope/src/Command/InstallCommand.php
@@ -13,8 +13,6 @@ namespace FriendsOfHyperf\Telescope\Command;
 
 use Hyperf\Command\Command;
 use Psr\Container\ContainerInterface;
-use Symfony\Component\Console\Input\ArrayInput;
-use Symfony\Component\Console\Output\NullOutput;
 
 class InstallCommand extends Command
 {
@@ -25,23 +23,13 @@ class InstallCommand extends Command
 
     public function handle()
     {
-        /** @var \Symfony\Component\Console\Application $application */
-        $application = $this->container->get(\Hyperf\Contract\ApplicationInterface::class);
-        $application->setAutoExit(false);
-
-        $output = new NullOutput();
-
-        $input = new ArrayInput(['command' => 'vendor:publish', 'package' => 'friendsofhyperf/telescope']);
-        $exitCode = $application->run($input, $output);
-        if (! $exitCode) {
+        if (! $this->call('vendor:publish', ['package' => 'laravel/telescope'])) {
             $this->info('publish successfully');
         } else {
             $this->error('publish failed');
         }
 
-        $input = new ArrayInput(['command' => 'migrate']);
-        $exitCode = $application->run($input, $output);
-        if (! $exitCode) {
+        if (! $this->call('migrate')) {
             $this->info('migrate successfully');
         } else {
             $this->error('migrate failed');
