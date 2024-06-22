f9659e925fb7c4dac3ee5693140f4e5320bfc5e3
10951 <guandeng@addcn.com>
2024-06-22 19:46:27 +0800

    Optimizing the code

--- src/telescope/src/Controller/RecordingController.php
+++ src/telescope/src/Controller/RecordingController.php
@@ -11,31 +11,23 @@ declare(strict_types=1);
 
 namespace FriendsOfHyperf\Telescope\Controller;
 
-use Hyperf\Contract\ContainerInterface;
+use FriendsOfHyperf\Telescope\Telescope;
 use Hyperf\HttpServer\Annotation\Controller;
 use Hyperf\HttpServer\Annotation\PostMapping;
-use Psr\SimpleCache\CacheInterface;
 
 #[Controller(server: 'telescope')]
 class RecordingController
 {
-    private CacheInterface $cache;
-
-    public function __construct(ContainerInterface $container)
-    {
-        $this->cache = $container->get(CacheInterface::class);
-    }
-
     /**
      * Toggle recording.
      */
     #[PostMapping(path: '/telescope/telescope-api/toggle-recording')]
     public function toggle(): void
     {
-        if ($this->cache->get('telescope:pause-recording')) {
-            $this->cache->delete('telescope:pause-recording');
+        if (Telescope::getCache()->get('telescope:pause-recording')) {
+            Telescope::getCache()->delete('telescope:pause-recording');
         } else {
-            $this->cache->set('telescope:pause-recording', true, 30 * 86400);
+            Telescope::getCache()->set('telescope:pause-recording', true, 30 * 86400);
         }
     }
 }
