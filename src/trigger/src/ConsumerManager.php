5698c9762d1201dda2a1ba8410aa48384c233225
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2024-09-12 08:38:15 +0800

    Refactor ConsumerManager

--- src/trigger/src/ConsumerManager.php
+++ src/trigger/src/ConsumerManager.php
@@ -29,6 +29,7 @@ class ConsumerManager
 
     public function run()
     {
+        /** @var array<string,array> */
         $connections = $this->config->get('trigger.connections', []);
 
         foreach ($connections as $connection => $options) {
@@ -36,31 +37,31 @@ class ConsumerManager
                 continue;
             }
 
-            $consumer = make(Consumer::class, [
-                'connection' => $connection,
-                'options' => (array) $options,
-                'logger' => $this->container->has(LoggerInterface::class) ? $this->container->get(LoggerInterface::class) : null,
-            ]);
-
-            $process = $this->createProcess($consumer);
-            $process->name = $consumer->getName();
-            $process->nums = 1;
+            $process = $this->createProcess($connection, $options);
 
             ProcessManager::register($process);
         }
     }
 
-    protected function createProcess(Consumer $consumer): AbstractProcess
+    protected function createProcess(string $connection, array $options = []): AbstractProcess
     {
-        return new class($this->container, $consumer) extends AbstractProcess {
-            public function __construct(ContainerInterface $container, protected Consumer $consumer)
+        return new class($this->container, $connection, $options) extends AbstractProcess {
+            public function __construct(ContainerInterface $container, protected string $connection, protected array $options)
             {
                 parent::__construct($container);
+                $this->name = $options['name'] ?? 'trigger' . $connection;
+                $this->nums = 1;
             }
 
             public function handle(): void
             {
-                $this->consumer->start();
+                $consumer = make(Consumer::class, [
+                    'connection' => $this->connection,
+                    'options' => (array) $this->options,
+                    'logger' => $this->container->has(LoggerInterface::class) ? $this->container->get(LoggerInterface::class) : null,
+                ]);
+
+                $consumer->start();
             }
         };
     }
