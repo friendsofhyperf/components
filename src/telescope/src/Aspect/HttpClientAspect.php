e563cfce854bbd34d259277c640dee6b6a9523a6
guandeng <guandeng@Hotmail.com>
2024-09-29 17:28:05 +0800

    Enhance HttpClientAspect to capture response body and allow multiple reads (#719)

--- src/telescope/src/Aspect/HttpClientAspect.php
+++ src/telescope/src/Aspect/HttpClientAspect.php
@@ -14,10 +14,14 @@ namespace FriendsOfHyperf\Telescope\Aspect;
 use FriendsOfHyperf\Telescope\IncomingEntry;
 use FriendsOfHyperf\Telescope\Telescope;
 use FriendsOfHyperf\Telescope\TelescopeConfig;
+use FriendsOfHyperf\Telescope\TelescopeContext;
 use GuzzleHttp\Client;
+use Hyperf\Collection\Arr;
 use Hyperf\Di\Aop\AbstractAspect;
 use Hyperf\Di\Aop\ProceedingJoinPoint;
+use Hyperf\Stringable\Str;
 use Psr\Http\Message\ResponseInterface;
+use Throwable;
 
 /**
  * @property array $config
@@ -38,6 +42,7 @@ class HttpClientAspect extends AbstractAspect
         if (! $this->telescopeConfig->isEnable('guzzle')) {
             return $proceedingJoinPoint->process();
         }
+
         $startTime = microtime(true);
         $instance = $proceedingJoinPoint->getInstance();
         $arguments = $proceedingJoinPoint->arguments;
@@ -57,15 +62,17 @@ class HttpClientAspect extends AbstractAspect
         $method = $arguments['keys']['method'] ?? 'GET';
         $uri = $arguments['keys']['uri'] ?? '';
         $headers = $options['headers'] ?? [];
-
         $result = $proceedingJoinPoint->process();
-
         $response = [];
+
         if ($result instanceof ResponseInterface) {
             $response['status'] = $result->getStatusCode();
             $response['reason'] = $result->getReasonPhrase();
             $response['headers'] = $result->getHeaders();
+            $response['body'] = $this->getResponsePayload($result);
+            $result->getBody()->rewind();
         }
+
         Telescope::recordClientRequest(IncomingEntry::make([
             'method' => $method,
             'uri' => $uri,
@@ -75,6 +82,67 @@ class HttpClientAspect extends AbstractAspect
             'response' => $response,
             'duration' => floor((microtime(true) - $startTime) * 1000),
         ]));
+
         return $result;
+    }
+
+    public function getResponsePayload(ResponseInterface $response)
+    {
+        $stream = $response->getBody();
+        try {
+            if ($stream->isSeekable()) {
+                $stream->rewind();
+            }
+
+            $content = $stream->getContents();
+        } catch (Throwable $e) {
+            return 'Purged By Hyperf Telescope: ' . $e->getMessage();
+        }
+
+        if (is_string($content)) {
+            if (! $this->contentWithinLimits($content)) {
+                return 'Purged By Hyperf Telescope';
+            }
+            if (
+                is_array(json_decode($content, true))
+                && json_last_error() === JSON_ERROR_NONE
+            ) {
+                return $this->contentWithinLimits($content) /* @phpstan-ignore-line */
+                ? $this->hideParameters(json_decode($content, true), Telescope::$hiddenResponseParameters)
+                : 'Purged By Hyperf Telescope';
+            }
+            if (Str::startsWith(strtolower($response->getHeaderLine('content-type') ?: ''), 'text/plain')) {
+                return $this->contentWithinLimits($content) ? $content : 'Purged By Hyperf Telescope'; /* @phpstan-ignore-line */
+            }
+            if (Str::contains($response->getHeaderLine('content-type'), 'application/grpc') !== false) {
+                return TelescopeContext::getGrpcResponsePayload() ?: 'Purged By Hyperf Telescope';
+            }
+        }
+
+        if (empty($content)) {
+            return 'Empty Response';
+        }
+
+        return 'HTML Response';
+    }
+
+    protected function contentWithinLimits(string $content): bool
+    {
+        $limit = 64;
+        return mb_strlen($content) / 1000 <= $limit;
+    }
+
+    /**
+     * Hide the given parameters.
+     */
+    protected function hideParameters(array $data, array $hidden): array
+    {
+        foreach ($hidden as $parameter) {
+            if (Arr::get($data, $parameter)) {
+                Arr::set($data, $parameter, '********');
+            }
+        }
+
+        return $data;
     }
 }
