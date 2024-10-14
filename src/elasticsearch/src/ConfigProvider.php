3df7888159b0eee859260e60c362b73284535bc8
Deeka Wong <huangdijia@gmail.com>
2024-10-14 14:30:03 +0800

    Adds Elasticsearch facade and config (#722)
    * Adds Elasticsearch facade and config
    
    * 更新 cnb.yaml 中的 git push 命令，强制推送以确保同步
    
    ---------
    
    Co-authored-by: Deeka Wong <8337659+huangdijia@users.noreply.github.com>

---
+++ src/elasticsearch/src/ConfigProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of friendsofhyperf/components.
+ *
+ * @link     https://github.com/friendsofhyperf/components
+ * @document https://github.com/friendsofhyperf/components/blob/main/README.md
+ * @contact  huangdijia@gmail.com
+ */
+
+namespace FriendsOfHyperf\Elasticsearch;
+
+class ConfigProvider
+{
+    public function __invoke()
+    {
+        return [
+            'publish' => [
+                [
+                    'id' => 'config',
+                    'description' => 'The config for elasticsearch.',
+                    'source' => __DIR__ . '/../publish/elasticsearch.php',
+                    'destination' => BASE_PATH . '/config/autoload/elasticsearch.php',
+                ],
+            ],
+        ];
+    }
+}
