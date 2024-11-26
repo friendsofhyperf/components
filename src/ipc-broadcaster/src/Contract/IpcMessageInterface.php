f0e40ad9ad23d56ab183ae43bd97deff6e99dc9d
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2024-11-26 14:14:33 +0800

    Add `ipc-broadcaster` component

---
+++ src/ipc-broadcaster/src/Contract/IpcMessageInterface.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of friendsofhyperf/components.
+ *
+ * @link     https://github.com/friendsofhyperf/components
+ * @document https://github.com/friendsofhyperf/components/blob/main/README.md
+ * @contact  huangdijia@gmail.com
+ */
+
+namespace FriendsOfHyperf\IpcBroadcaster\Contract;
+
+interface IpcMessageInterface
+{
+    public function handle(): void;
+
+    public function setWorkerId(int $workerId): void;
+}
