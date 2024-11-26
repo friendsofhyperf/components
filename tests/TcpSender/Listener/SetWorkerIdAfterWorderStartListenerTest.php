f19b7892eb7e615fd3ddbb8df27c279777393950
Deeka Wong <huangdijia@gmail.com>
2024-11-26 21:17:47 +0800

    Improve `tcp-sender` (#750)
    * Improve `tcp-sender` component
    
    * 删除 TcpSender 组件的测试文件，并添加 SetWorkerIdAfterWorderStartListener 的新测试
    
    * 更新 TcpSender 测试，添加 BroadcasterInterface 的模拟并移除冗余测试
    
    ---------
    
    Co-authored-by: Deeka Wong <8337659+huangdijia@users.noreply.github.com>

--- tests/TcpSender/Listener/SetWorkerIdAfterWorderStartListenerTest.php
+++ tests/TcpSender/Listener/SetWorkerIdAfterWorderStartListenerTest.php
@@ -11,7 +11,7 @@ declare(strict_types=1);
 
 namespace FriendsOfHyperf\Tests\TcpSender\Listener;
 
-use FriendsOfHyperf\TcpSender\Listener\InitSenderListener;
+use FriendsOfHyperf\TcpSender\Listener\SetWorkerIdAfterWorderStartListener;
 use FriendsOfHyperf\TcpSender\Sender;
 use Hyperf\Contract\ContainerInterface;
 use Hyperf\Framework\Event\AfterWorkerStart;
@@ -23,9 +23,9 @@ use Swoole\Server;
  * @coversNothing
  */
 #[\PHPUnit\Framework\Attributes\Group('tcp-sender')]
-class InitSenderListenerTest extends TestCase
+class SetWorkerIdAfterWorderStartListenerTest extends TestCase
 {
-    private InitSenderListener $listener;
+    private SetWorkerIdAfterWorderStartListener $listener;
 
     private ContainerInterface $container;
 
@@ -38,7 +38,7 @@ class InitSenderListenerTest extends TestCase
         $this->container = $this->createMock(ContainerInterface::class);
         $this->sender = $this->createMock(Sender::class);
         $this->server = $this->createMock(Server::class);
-        $this->listener = new InitSenderListener($this->container);
+        $this->listener = new SetWorkerIdAfterWorderStartListener($this->container);
     }
 
     public function testSenderIsSetWithWorkerIdWhenContainerHasSender(): void
