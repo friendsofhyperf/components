20b4df4f5c82978f9407f71cd90a2833d89bf775
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2024-11-29 21:03:37 +0800

    fix: 更新导航链接以支持英文版本

--- docs/.vitepress/src/en/nav.ts
+++ docs/.vitepress/src/en/nav.ts
@@ -1,9 +1,9 @@
 import {DefaultTheme} from "vitepress";
 
 const nav:DefaultTheme.NavItem[] = [
-    // { text: 'Guide', link: '/zh/guide/' },
-    { text: 'Components', link: '/zh/components/' },
-    { text: 'FAQ', link: '/zh/faq/index' },
+    // { text: 'Guide', link: '/en/guide/' },
+    { text: 'Components', link: '/en/components/' },
+    { text: 'FAQ', link: '/en/faq/index' },
     { text: 'Hyperf', link: 'https://hyperf.wiki/' }
 ]
 
