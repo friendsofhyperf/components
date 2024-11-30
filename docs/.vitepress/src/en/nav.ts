b444e4b99bfc5bc3a7300c246e1e47ebbb9ac527
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2024-11-30 18:49:40 +0800

    feat: 在导航中添加 MineAdmin 组件链接

--- docs/.vitepress/src/en/nav.ts
+++ docs/.vitepress/src/en/nav.ts
@@ -4,7 +4,8 @@ const nav:DefaultTheme.NavItem[] = [
     // { text: 'Guide', link: '/zh_CN/guide/' },
     { text: 'Components', link: '/zh_CN/components/' },
     { text: 'FAQ', link: '/zh_CN/faq/index' },
-    { text: 'Hyperf', link: 'https://hyperf.wiki/#/en/' }
+    { text: 'Hyperf', link: 'https://hyperf.wiki/#/en/' },
+    { text: 'MineAdmin', link: 'https://www.mineadmin.com/' }
 ]
 
 export default nav
--- docs/.vitepress/src/zh/nav.ts
+++ docs/.vitepress/src/zh/nav.ts
@@ -4,7 +4,8 @@ const nav:DefaultTheme.NavItem[] = [
     // { text: '入门', link: '/zh_CN/guide/' },
     { text: '组件', link: '/zh_CN/components/' },
     { text: 'FAQ', link: '/zh_CN/faq/index' },
-    { text: 'Hyperf', link: 'https://hyperf.wiki/' }
+    { text: 'Hyperf', link: 'https://hyperf.wiki/' },
+    { text: 'MineAdmin', link: 'https://www.mineadmin.com/' }
 ]
 
 export default nav
