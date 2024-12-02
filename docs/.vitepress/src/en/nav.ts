4494d1507d95f3451c2d7e4f2b1cff924dd15556
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2024-12-02 17:14:52 +0800

    fix: 更新导航菜单，调整链接结构并添加更多推荐项

--- docs/.vitepress/src/en/nav.ts
+++ docs/.vitepress/src/en/nav.ts
@@ -1,11 +1,14 @@
 import {DefaultTheme} from "vitepress";
 
-const nav:DefaultTheme.NavItem[] = [
+const nav: DefaultTheme.NavItem[] = [
     // { text: 'Guide', link: '/en/guide/' },
     { text: 'Components', link: '/en/components/' },
     { text: 'FAQ', link: '/en/faq/index' },
-    { text: 'Hyperf', link: 'https://hyperf.wiki/#/en/' },
-    { text: 'MineAdmin', link: 'https://www.mineadmin.com/' }
+    { text: 'More', items: [
+            { text: 'Hyperf', link: 'https://hyperf.wiki/' },
+            { text: 'MineAdmin', link: 'https://www.mineadmin.com/' }
+        ]
+    }
 ]
 
 export default nav
