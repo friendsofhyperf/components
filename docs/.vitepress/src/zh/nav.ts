945ef35fbb746fe63dbcc5b1698f26a0e7d6c2c0
lyan <49744633+zds-s@users.noreply.github.com>
2024-12-02 17:15:26 +0800

    Update nav.ts (#763)
    * Update nav.ts
    
    * fix: 更新导航菜单，调整链接结构并添加更多推荐项
    
    ---------
    
    Co-authored-by: Deeka Wong <8337659+huangdijia@users.noreply.github.com>

--- docs/.vitepress/src/zh/nav.ts
+++ docs/.vitepress/src/zh/nav.ts
@@ -4,8 +4,11 @@ const nav:DefaultTheme.NavItem[] = [
     // { text: '入门', link: '/zh_CN/guide/' },
     { text: '组件', link: '/zh_CN/components/' },
     { text: 'FAQ', link: '/zh_CN/faq/index' },
-    { text: 'Hyperf', link: 'https://hyperf.wiki/' },
-    { text: 'MineAdmin', link: 'https://www.mineadmin.com/' }
+    { text: '更多', items:[
+            { text: 'Hyperf', link: 'https://hyperf.wiki/' },
+            { text: 'MineAdmin', link: 'https://www.mineadmin.com/' }
+        ]
+    }
 ]
 
 export default nav
