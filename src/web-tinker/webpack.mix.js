56d29409d62381cf36f7a1db345726a51e859e4f
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2024-12-04 15:27:00 +0800

    Revert "feat: 添加 mix-manifest.json 文件并更新资源版本，优化构建配置"
    This reverts commit 47f8dd0ad431093c6734bfd48fb403b5800168a2.

--- src/web-tinker/webpack.mix.js
+++ src/web-tinker/webpack.mix.js
@@ -13,4 +13,5 @@ mix
         // Our PostCSS plugins are defined in a standard `postcss.config.js`
         // file, which we'll read for plugins.
         postCss: require('./postcss.config').plugins,
-    });
+    })
+    .copy('public', '../web-tinker-app/public/vendor/web-tinker');
