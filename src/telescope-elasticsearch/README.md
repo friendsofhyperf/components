67d50769aad911692696e159a6d536f0fb54e202
Deeka Wong <huangdijia@gmail.com>
2024-12-10 20:19:44 +0800

    feat: add `telescope-elasticsearch` component (#785)

---
+++ src/telescope-elasticsearch/README.md
@@ -0,0 +1,13 @@
+# Telescope Elasticsearch Driver
+
+[![Latest Version](https://img.shields.io/packagist/v/friendsofhyperf/telescope-elasticsearch.svg?style=flat-square)](https://packagist.org/packages/friendsofhyperf/telescope-elasticsearch)
+[![Total Downloads](https://img.shields.io/packagist/dt/friendsofhyperf/telescope-elasticsearch.svg?style=flat-square)](https://packagist.org/packages/friendsofhyperf/telescope-elasticsearch)
+[![GitHub license](https://img.shields.io/github/license/friendsofhyperf/telescope-elasticsearch)](https://github.com/friendsofhyperf/telescope-elasticsearch)
+
+It will allow you to switch from an SQL database to Elasticsearch as a driver for your data storage, and it will eliminate deadlocks, making Telescope a ready-for-production logging system.
+
+## Installation
+
+```shell
+composer require friendsofhyperf/telescope-elasticsearch
+```
---
+++ src/telescope-elasticsearch/src/Storage/EntriesIndex.php
@@ -0,0 +1,160 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of friendsofhyperf/components.
+ *
+ * @link     https://github.com/friendsofhyperf/components
+ * @document https://github.com/friendsofhyperf/components/blob/main/README.md
+ * @contact  huangdijia@gmail.com
+ */
+
+namespace FriendsOfHyperf\TelescopeElasticsearch\Storage;
+
+use Elastic\Elasticsearch\Client;
+use Elastic\Elasticsearch\Exception\ClientResponseException;
+use Elastic\Elasticsearch\Exception\MissingParameterException;
+use Elastic\Elasticsearch\Exception\ServerResponseException;
+use Elastic\Elasticsearch\Response\Elasticsearch;
+use FriendsOfHyperf\Elasticsearch\ClientBuilderFactory;
+use Http\Promise\Promise;
+use Hyperf\Contract\StdoutLoggerInterface;
+
+class EntriesIndex
+{
+    public function __construct(
+        private ClientBuilderFactory $clientBuilderFactory,
+        public string $index = 'telescope_entries',
+        private array $options = [],
+        private ?StdoutLoggerInterface $logger = null,
+    ) {
+    }
+
+    /**
+     * @return Elasticsearch|Promise|void
+     */
+    public function create()
+    {
+        try {
+            return $this->client()->indices()->create([
+                'index' => $this->index,
+                'body' => [
+                    'settings' => [
+                        'index' => [
+                            'number_of_shards' => 1,
+                            'number_of_replicas' => 0,
+                        ],
+                    ],
+                    'mappings' => [
+                        '_source' => [
+                            'enabled' => true,
+                        ],
+                        'properties' => $this->properties(),
+                    ],
+                ],
+            ]);
+        } catch (ClientResponseException $e) {
+            $this->logger?->error('the 4xx error', ['message' => $e->getMessage()]);
+        } catch (MissingParameterException $e) {
+            $this->logger?->error('the 5xx error', ['message' => $e->getMessage()]);
+        } catch (ServerResponseException $e) {
+            $this->logger?->error('network error like NoNodeAvailableException', ['message' => $e->getMessage()]);
+        }
+    }
+
+    public function properties(): array
+    {
+        return [
+            'uuid' => [
+                'type' => 'keyword',
+            ],
+            'batch_id' => [
+                'type' => 'keyword',
+            ],
+            'family_hash' => [
+                'type' => 'keyword',
+            ],
+            'should_display_on_index' => [
+                'type' => 'boolean',
+                'null_value' => true,
+            ],
+            'type' => [
+                'type' => 'keyword',
+            ],
+            'content' => [
+                'type' => 'object',
+                'dynamic' => false,
+            ],
+            'tags' => [
+                'type' => 'nested',
+                'dynamic' => false,
+                'properties' => [
+                    'raw' => [
+                        'type' => 'keyword',
+                    ],
+                    'name' => [
+                        'type' => 'keyword',
+                    ],
+                    'value' => [
+                        'type' => 'keyword',
+                    ],
+                ],
+            ],
+            'created_at' => [
+                'type' => 'date',
+                'format' => 'yyyy-MM-dd HH:mm:ss',
+            ],
+            '@timestamp' => [
+                'type' => 'date',
+            ],
+        ];
+    }
+
+    public function delete(): void
+    {
+        try {
+            $this->client()->indices()->delete([
+                'index' => $this->index,
+            ]);
+        } catch (ClientResponseException $e) {
+            $this->logger?->error('the 4xx error', ['message' => $e->getMessage()]);
+        } catch (MissingParameterException $e) {
+            $this->logger?->error('the 5xx error', ['message' => $e->getMessage()]);
+        } catch (ServerResponseException $e) {
+            $this->logger?->error('network error like NoNodeAvailableException', ['message' => $e->getMessage()]);
+        }
+    }
+
+    public function exists(): bool
+    {
+        try {
+            return $this->client()
+                ->indices()
+                ->exists([
+                    'index' => $this->index,
+                ])
+                ->getStatusCode() !== 404;
+        } catch (ClientResponseException $e) {
+            $this->logger?->error('the 4xx error', ['message' => $e->getMessage()]);
+        } catch (MissingParameterException $e) {
+            $this->logger?->error('the 5xx error', ['message' => $e->getMessage()]);
+        } catch (ServerResponseException $e) {
+            $this->logger?->error('network error like NoNodeAvailableException', ['message' => $e->getMessage()]);
+        }
+
+        return false;
+    }
+
+    public function client(): Client
+    {
+        $options = $this->options;
+        $clientBuilderFactory = $this->clientBuilderFactory->create([]);
+        if (isset($options['hosts'])) {
+            $clientBuilderFactory->setHosts($options['hosts']);
+        }
+        if (isset($options['username'], $options['password'])) {
+            $clientBuilderFactory->setBasicAuthentication($options['username'], $options['password']);
+        }
+        return $clientBuilderFactory->create()->build();
+    }
+}
