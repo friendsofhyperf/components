7b7459f14b775d5948327fd9472e02bdaf6ff95f
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2024-12-26 17:29:28 +0800

    将导航中的“FAQ”文本更改为“常见问题”，以提高中文用户的理解度

--- docs/.vitepress/src/zh-cn/nav.ts
+++ docs/.vitepress/src/zh-cn/nav.ts
@@ -2,7 +2,7 @@ import {DefaultTheme} from "vitepress";
 
 const nav:DefaultTheme.NavItem[] = [
     { text: '组件', link: '/zh-cn/components/' },
-    { text: 'FAQ', link: '/zh-cn/faq/index' },
+    { text: '常见问题', link: '/zh-cn/faq/index' },
     { text: '更多', items:[
             { text: 'Hyperf', link: 'https://hyperf.wiki/' },
             { text: 'MineAdmin', link: 'https://www.mineadmin.com/' }
