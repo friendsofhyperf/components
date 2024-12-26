e7e73e2b671bd09ca91b418424f37fcea97909a3
huangdijia <huangdijia@users.noreply.github.com>
2024-12-26 09:30:28 +0000

    Update docs and translate

--- docs/.vitepress/src/zh-hk/nav.ts
+++ docs/.vitepress/src/zh-hk/nav.ts
@@ -2,7 +2,7 @@ import {DefaultTheme} from "vitepress";
 
 const nav:DefaultTheme.NavItem[] = [
     { text: '組件', link: '/zh-hk/components/' },
-    { text: 'FAQ', link: '/zh-hk/faq/index' },
+    { text: '常見問題', link: '/zh-hk/faq/index' },
     { text: '更多', items:[
             { text: 'Hyperf', link: 'https://hyperf.wiki/' },
             { text: 'MineAdmin', link: 'https://www.mineadmin.com/' }
