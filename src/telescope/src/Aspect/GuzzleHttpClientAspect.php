b5f0d880da5979770a664163667bab57f14d7f29
Deeka Wong <huangdijia@gmail.com>
2025-02-08 21:42:49 +0800

    Optimized `GuzzleHttpClientAspect` (#837)
    * Optimized `GuzzleHttpClientAspect`
    
    * 优化 GuzzleHttpClientAspect 中的响应数据记录逻辑
    
    ---------
    
    Co-authored-by: Deeka Wong <8337659+huangdijia@users.noreply.github.com>

--- src/telescope/src/Aspect/GuzzleHttpClientAspect.php
+++ src/telescope/src/Aspect/GuzzleHttpClientAspect.php
@@ -64,23 +64,22 @@ class GuzzleHttpClientAspect extends AbstractAspect
             try {
                 $request = $stats->getRequest();
                 $response = $stats->getResponse();
-                $data = [
-                    'status' => $response->getStatusCode(),
-                    'reason' => $response->getReasonPhrase(),
-                    'headers' => $response->getHeaders(),
-                    'body' => $this->getResponsePayload($response),
-                ];
-
-                Telescope::recordClientRequest(IncomingEntry::make([
+                $content = [
                     'method' => $request->getMethod(),
                     'uri' => $request->getUri()->__toString(),
                     'headers' => $request->getHeaders(),
-                    'response_status' => $data['status'],
-                    'response_headers' => $data['headers'],
-                    'response_data' => $data,
                     'duration' => $stats->getTransferTime() * 1000,
-                ]));
-            } catch (Throwable $e) {
+                ];
+
+                if ($response = $stats->getResponse()) {
+                    $content['response_status'] = $response->getStatusCode();
+                    $content['response_headers'] = $response->getHeaders();
+                    $content['response_reason'] = $response->getReasonPhrase();
+                    $content['response_payload'] = $this->getResponsePayload($response);
+                }
+
+                Telescope::recordClientRequest(IncomingEntry::make($content));
+            } catch (Throwable $exception) {
                 // We will catch the exception to prevent the request from being interrupted.
             }
 
