8207101ffd87813d5843e6193eddd8355c60934d
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2025-04-30 18:52:10 +0800

    fix(tests): 在 CarbonCastTest 中为 'casts to carbon' 测试添加 PHP 8.3.20 跳过注解

--- tests/ValidatedDTO/Unit/CarbonCastTest.php
+++ tests/ValidatedDTO/Unit/CarbonCastTest.php
@@ -14,6 +14,9 @@ use FriendsOfHyperf\ValidatedDTO\Casting\CarbonCast;
 use FriendsOfHyperf\ValidatedDTO\Casting\CarbonImmutableCast;
 use FriendsOfHyperf\ValidatedDTO\Exception\CastException;
 
+beforeEach(function () {
+})->skipOnPhp('8.3.20');
+
 it('casts to carbon', function () {
     $castable = new CarbonCast();
 
