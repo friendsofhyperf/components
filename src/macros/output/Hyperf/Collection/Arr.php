9afc9fd29bb30f590a2b9ec6aa7347cf52875400
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2025-04-30 16:59:02 +0800

    Typed getters for Arr helper

--- src/macros/output/Hyperf/Collection/Arr.php
+++ src/macros/output/Hyperf/Collection/Arr.php
@@ -11,8 +11,56 @@ declare(strict_types=1);
 
 namespace Hyperf\Collection;
 
+use ArrayAccess;
+use InvalidArgumentException;
+
 class Arr
 {
+    /**
+     * Get an array item from an array using "dot" notation.
+     * @return array
+     * @throws InvalidArgumentException
+     */
+    public static function array(ArrayAccess|array $array, string|int|null $key, ?array $default = null)
+    {
+    }
+
+    /**
+     * Get a boolean item from an array using "dot" notation.
+     * @return bool
+     * @throws InvalidArgumentException
+     */
+    public static function boolean(ArrayAccess|array $array, string|int|null $key, ?bool $default = null)
+    {
+    }
+
+    /**
+     * Get a float item from an array using "dot" notation.
+     * @return float
+     * @throws InvalidArgumentException
+     */
+    public static function float(ArrayAccess|array $array, string|int|null $key, ?float $default = null)
+    {
+    }
+
+    /**
+     * Get an integer item from an array using "dot" notation.
+     * @return int
+     * @throws InvalidArgumentException
+     */
+    public static function integer(ArrayAccess|array $array, string|int|null $key, ?int $default = null)
+    {
+    }
+
+    /**
+     * Get a string item from an array using "dot" notation.
+     * @return string
+     * @throws InvalidArgumentException
+     */
+    public static function string(ArrayAccess|array $array, string|int|null $key, ?string $default = null)
+    {
+    }
+
     /**
      * Sort given array by many properties.
      *
--- src/macros/src/ArrMixin.php
+++ src/macros/src/ArrMixin.php
@@ -11,13 +11,102 @@ declare(strict_types=1);
 
 namespace FriendsOfHyperf\Macros;
 
+use ArrayAccess;
 use Hyperf\Collection\Arr;
+use InvalidArgumentException;
 
 /**
  * @mixin Arr
  */
 class ArrMixin
 {
+    public static function array()
+    {
+        return function (ArrayAccess|array $array, string|int|null $key, ?array $default = null) {
+            $value = Arr::get($array, $key, $default);
+
+            if (! is_array($value)) {
+                throw new InvalidArgumentException(
+                    sprintf('Array value for key [%s] must be an array, %s found.', $key, gettype($value))
+                );
+            }
+
+            return $value;
+        };
+    }
+
+    /**
+     * Get a boolean item from an array using "dot" notation.
+     */
+    public static function boolean()
+    {
+        return function (ArrayAccess|array $array, string|int|null $key, ?bool $default = null) {
+            $value = Arr::get($array, $key, $default);
+
+            if (! is_bool($value)) {
+                throw new InvalidArgumentException(
+                    sprintf('Array value for key [%s] must be a boolean, %s found.', $key, gettype($value))
+                );
+            }
+
+            return $value;
+        };
+    }
+
+    /**
+     * Get a float item from an array using "dot" notation.
+     */
+    public static function float()
+    {
+        return function (ArrayAccess|array $array, string|int|null $key, ?float $default = null) {
+            $value = Arr::get($array, $key, $default);
+
+            if (! is_float($value)) {
+                throw new InvalidArgumentException(
+                    sprintf('Array value for key [%s] must be a float, %s found.', $key, gettype($value))
+                );
+            }
+
+            return $value;
+        };
+    }
+
+    /**
+     * Get an integer item from an array using "dot" notation.
+     */
+    public static function integer()
+    {
+        return function (ArrayAccess|array $array, string|int|null $key, ?int $default = null) {
+            $value = Arr::get($array, $key, $default);
+
+            if (! is_integer($value)) {
+                throw new InvalidArgumentException(
+                    sprintf('Array value for key [%s] must be an integer, %s found.', $key, gettype($value))
+                );
+            }
+
+            return $value;
+        };
+    }
+
+    /**
+     * Get a string item from an array using "dot" notation.
+     */
+    public static function string()
+    {
+        return function (ArrayAccess|array $array, string|int|null $key, ?string $default = null) {
+            $value = Arr::get($array, $key, $default);
+
+            if (! is_string($value)) {
+                throw new InvalidArgumentException(
+                    sprintf('Array value for key [%s] must be a string, %s found.', $key, gettype($value))
+                );
+            }
+
+            return $value;
+        };
+    }
+
     public function sortByMany()
     {
         return function ($array, $comparisons = []) {
