7058af1547064d515fd5e245bdde2842a96b4032
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2025-09-21 09:19:10 +0800

    refactor(sentry): reorganize filesystem methods and enhance breadcrumb data structure

--- src/sentry/src/Aspect/FilesystemAspect.php
+++ src/sentry/src/Aspect/FilesystemAspect.php
@@ -20,24 +20,22 @@ use Sentry\Breadcrumb;
 class FilesystemAspect extends AbstractAspect
 {
     public $classes = [
+        'League\Flysystem\*\*Adapter::directoryExists',
         'League\Flysystem\*\*Adapter::write',
         'League\Flysystem\*\*Adapter::writeStream',
-        'League\Flysystem\*\*Adapter::setVisibility',
+        'League\Flysystem\*\*Adapter::read',
+        'League\Flysystem\*\*Adapter::readStream',
         'League\Flysystem\*\*Adapter::delete',
         'League\Flysystem\*\*Adapter::deleteDirectory',
         'League\Flysystem\*\*Adapter::createDirectory',
-        'League\Flysystem\*\*Adapter::move',
-        'League\Flysystem\*\*Adapter::copy',
-        'League\Flysystem\*\*Adapter::fileExists',
-        'League\Flysystem\*\*Adapter::directoryExists',
-        'League\Flysystem\*\*Adapter::has',
-        'League\Flysystem\*\*Adapter::read',
-        'League\Flysystem\*\*Adapter::readStream',
-        'League\Flysystem\*\*Adapter::listContents',
+        'League\Flysystem\*\*Adapter::setVisibility',
+        'League\Flysystem\*\*Adapter::visibility',
+        'League\Flysystem\*\*Adapter::mimeType',
         'League\Flysystem\*\*Adapter::lastModified',
         'League\Flysystem\*\*Adapter::fileSize',
-        'League\Flysystem\*\*Adapter::mimeType',
-        'League\Flysystem\*\*Adapter::visibility',
+        'League\Flysystem\*\*Adapter::listContents',
+        'League\Flysystem\*\*Adapter::move',
+        'League\Flysystem\*\*Adapter::copy',
     ];
 
     public function __construct(
@@ -52,10 +50,59 @@ class FilesystemAspect extends AbstractAspect
         }
 
         $method = $proceedingJoinPoint->methodName;
+        $arguments = $proceedingJoinPoint->arguments['keys'] ?? [];
         // See https://develop.sentry.dev/sdk/performance/span-operations/#web-server
         $op = "file.{$method}";
-        $description = $proceedingJoinPoint->className . '::' . $method;
-        $data = [];
+        $description = match ($method) {
+            'move', 'copy' => sprintf(
+                'from "%s" to "%s"',
+                $arguments['source'] ?? '',
+                $arguments['destination'] ?? ''
+            ),
+            'write', 'writeStream',
+            'read', 'readStream',
+            'setVisibility', 'visibility',
+            'delete', 'deleteDirectory', 'createDirectory',
+            'fileExists', 'directoryExists',
+            'listContents',
+            'lastModified',
+            'fileSize',
+            'mimeType', => $arguments['path'] ?? '',
+            default => '',
+        };
+        $data = match ($method) {
+            'move', 'copy' => [
+                'from' => $arguments['source'] ?? '',
+                'to' => $arguments['destination'] ?? '',
+            ],
+            'write', 'writeStream' => [
+                'path' => $arguments['path'] ?? '',
+                'config' => $arguments['config'] ?? null,
+            ],
+            'read', 'readStream' => [
+                'path' => $arguments['path'] ?? '',
+            ],
+            'setVisibility' => [
+                'path' => $arguments['path'] ?? '',
+                'visibility' => $arguments['visibility'] ?? '',
+            ],
+            'visibility',
+            'delete', 'deleteDirectory' => [
+                'path' => $arguments['path'] ?? '',
+            ],
+            'createDirectory' => [
+                'path' => $arguments['path'] ?? '',
+                'config' => $arguments['config'] ?? null,
+            ],
+            'fileExists', 'directoryExists',
+            'listContents',
+            'lastModified',
+            'fileSize',
+            'mimeType' => [
+                'path' => $arguments['path'] ?? '',
+            ],
+            default => [],
+        };
 
         Integration::addBreadcrumb(new Breadcrumb(
             Breadcrumb::LEVEL_INFO,
