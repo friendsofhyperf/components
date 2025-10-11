609ce982d13a3207b605a6f24f687499a14b4909
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2025-10-11 10:18:41 +0800

    refactor(rpc): standardize operation name for RPC calls in SpanContext

--- src/sentry/src/Tracing/Aspect/RpcAspect.php
+++ src/sentry/src/Tracing/Aspect/RpcAspect.php
@@ -76,7 +76,7 @@ class RpcAspect extends AbstractAspect
         };
 
         Context::set(static::SPAN_CONTEXT, SpanContext::make()
-            ->setOp(sprintf('%s.%s/%s', $package, $service, $path))
+            ->setOp('rpc.call')
             ->setDescription($path)
             ->setOrigin('auto.rpc')
             ->setData([
