118ed7c32f7c22964c7e2b03d62eb069465d865e
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2025-11-11 17:32:27 +0800

    更新依赖项，将 laravel/serializable-closure 版本从 ^1.0 升级到 ^2.0，并在支持组件中添加 async-queue-closure-job 依赖，同时添加 ClosureJob 类以兼容旧版本。

---
+++ src/support/src/AsyncQueue/ClosureJob.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of friendsofhyperf/components.
+ *
+ * @link     https://github.com/friendsofhyperf/components
+ * @document https://github.com/friendsofhyperf/components/blob/main/README.md
+ * @contact  huangdijia@gmail.com
+ */
+
+namespace FriendsOfHyperf\Support\AsyncQueue;
+
+/**
+ * @deprecated since v3.2, will be removed in v3.2, use `FriendsOfHyperf\AsyncQueueClosureJob\ClosureJob` instead.
+ */
+class ClosureJob extends \FriendsOfHyperf\AsyncQueueClosureJob\ClosureJob
+{
+}
