af2c45a2b04cbe52089b048e08812f76fc77af90
Deeka Wong <huangdijia@gmail.com>
2025-11-14 07:03:42 +0800

    更新 Functions.php

--- src/support/src/Functions.php
+++ src/support/src/Functions.php
@@ -24,7 +24,7 @@ use function Hyperf\Support\value;
  * Do not assign a value to the return value of this function unless you are very clear about the consequences of doing so.
  * @param Closure|JobInterface|ProduceMessage|ProducerMessageInterface|mixed $job
  * @return ($job is Closure ? PendingAsyncQueueDispatch : ($job is JobInterface ? PendingAsyncQueueDispatch : ($job is ProducerMessageInterface ? PendingAmqpProducerMessageDispatch : PendingKafkaProducerMessageDispatch)))
- * @throws InvalidArgumentException
+ * @throws \InvalidArgumentException
  */
 function dispatch($job)
 {
