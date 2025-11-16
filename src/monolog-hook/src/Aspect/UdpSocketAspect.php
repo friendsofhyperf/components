7e4c414650aa20047256d550b2ca93536ac32fb5
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2025-11-16 20:52:50 +0800

    refactor: simplify UdpSocketAspect by removing unnecessary properties and methods

--- src/monolog-hook/src/Aspect/UdpSocketAspect.php
+++ src/monolog-hook/src/Aspect/UdpSocketAspect.php
@@ -11,47 +11,10 @@ declare(strict_types=1);
 
 namespace FriendsOfHyperf\MonologHook\Aspect;
 
-use Hyperf\Coroutine\Coroutine;
-use Hyperf\Di\Aop\AbstractAspect;
-use Hyperf\Di\Aop\ProceedingJoinPoint;
-use Monolog\Handler\SyslogUdp\UdpSocket;
-use Socket;
-use WeakMap;
+class_alias(\Hyperf\Logger\Aspect\UdpSocketAspect::class, UdpSocketAspect::class);
 
-/**
- * @property null|Socket $socket
- */
-class UdpSocketAspect extends AbstractAspect
-{
-    public array $classes = [
-        UdpSocket::class . '::getSocket',
-    ];
-
-    public static WeakMap $coSockets;
-
-    public function __construct()
+if (! class_exists(UdpSocketAspect::class)) {
+    class UdpSocketAspect extends \Hyperf\Logger\Aspect\UdpSocketAspect
     {
-        self::$coSockets = new WeakMap();
-    }
-
-    public function process(ProceedingJoinPoint $proceedingJoinPoint)
-    {
-        if (! Coroutine::inCoroutine()) {
-            return $proceedingJoinPoint->process();
-        }
-
-        $instance = $proceedingJoinPoint->getInstance();
-
-        if (isset(self::$coSockets[$instance]) && self::$coSockets[$instance] instanceof Socket) {
-            return self::$coSockets[$instance];
-        }
-
-        return self::$coSockets[$instance] = (function () use ($proceedingJoinPoint) {
-            $nonCoSocket = $this->socket; // Save the socket of non-coroutine.
-            $this->socket = null; // Unset the socket of non-coroutine.
-            $coSocket = $proceedingJoinPoint->process(); // ReCreate the socket in coroutine.
-            $this->socket = $nonCoSocket; // Restore the socket of non-coroutine.
-            return $coSocket;
-        })->call($instance);
     }
 }
