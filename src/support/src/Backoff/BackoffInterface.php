658df7ecb376a49a91488f9e603b9ae4990ac1a4
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2025-12-05 18:01:03 +0800

    feat: 更新BackoffInterface接口文档，增加方法注释以提高可读性

--- src/support/src/Backoff/BackoffInterface.php
+++ src/support/src/Backoff/BackoffInterface.php
@@ -11,11 +11,29 @@ declare(strict_types=1);
 
 namespace FriendsOfHyperf\Support\Backoff;
 
+/**
+ * Backoff algorithm interface
+ * Used to implement delay time calculation in retry mechanisms.
+ */
 interface BackoffInterface
 {
+    /**
+     * Get the delay time for the next retry (milliseconds).
+     *
+     * @return int Delay time in milliseconds
+     */
     public function next(): int;
 
+    /**
+     * Reset backoff state
+     * Reset retry count and related state to initial values.
+     */
     public function reset(): void;
 
+    /**
+     * Get the current retry count.
+     *
+     * @return int Current number of retries
+     */
     public function getAttempt(): int;
 }
